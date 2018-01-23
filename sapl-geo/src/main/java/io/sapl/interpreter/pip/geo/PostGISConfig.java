6a9d7b3f9859b475594258dffa7ee618c773ea04
MariusMueller90 <marius.mueller@studium.fernuni-Hagen.de>
2018-01-23 16:32:14 +0100

    Coordinate transformation btw. GIS and JTS

--- sapl-geo/src/main/java/io/sapl/interpreter/pip/geo/PostGISConfig.java
+++ sapl-geo/src/main/java/io/sapl/interpreter/pip/geo/PostGISConfig.java
@@ -15,110 +15,109 @@ import lombok.Getter;
 @EqualsAndHashCode
 public class PostGISConfig {
 
-    private static final String ARG_NOT_EXISTING = "Configured table or column name does not exist in the database.";
-    private static final String COLUMN_NAME = "COLUMN_NAME";
-    private static final String JDBC_SERVICE = "jdbc:postgresql://";
-    private static final String SSL_PARAM = "ssl=true";
-    private static final String SQL_QUERY = "SELECT %s, %s FROM %s WHERE %s;";
-    private static final String SQL_PROJECTGEOM = "ST_AsText(ST_Transform(%s,%s))";
-    private static final String SQL_GEOM = "ST_AsText(%s)";
-    private static final String SQL_AND = " AND ";
-    private static final String SLASH = "/";
-    private static final String QM = "?";
-    private static final String AMP = "&";
-    private static final String SEQ = "<=";
-    private static final String GEQ = ">=";
-    private static final char COLON = ':';
+	private static final String ARG_NOT_EXISTING = "Configured table or column name does not exist in the database.";
+	private static final String COLUMN_NAME = "COLUMN_NAME";
+	private static final String JDBC_SERVICE = "jdbc:postgresql://";
+	private static final String SSL_PARAM = "ssl=true";
+	private static final String SQL_QUERY = "SELECT %s, %s FROM %s WHERE %s;";
+	private static final String SQL_PROJECTGEOM = "ST_AsText(ST_FlipCoordinates(ST_Transform(%s,%s)))";
+	private static final String SQL_GEOM = "ST_AsText(ST_FlipCoordinates(%s))";
+	private static final String SQL_AND = " AND ";
+	private static final String SLASH = "/";
+	private static final String QM = "?";
+	private static final String AMP = "&";
+	private static final String SEQ = "<=";
+	private static final String GEQ = ">=";
+	private static final char COLON = ':';
 
-    private String serverAdress;
-    private String port;
-    private String db;
-    private String table;
-    private String username;
-    private String password;
-    private String pkColName;
-    private String idColName;
-    private String geometryColName;
-    private int from;
-    private int until = -1;
-    private boolean enableProjection;
-    private int projectionSRID;
-    private boolean ssl;
-    private String urlParams;
+	private String serverAdress;
+	private String port;
+	private String db;
+	private String table;
+	private String username;
+	private String password;
+	private String pkColName;
+	private String idColName;
+	private String geometryColName;
+	private int from;
+	private int until = -1;
+	private boolean enableProjection;
+	private int projectionSRID;
+	private boolean ssl;
+	private String urlParams;
 
-    public Connection getConnection() throws SQLException {
-	return DriverManager.getConnection(buildUrl(), getUsername(), getPassword());
-    }
-
-    public String buildQuery() throws AttributeException {
-	if (verifySqlArguments()) {
-	    return String.format(SQL_QUERY, getIdColName(), buildGeometryExpression(), getTable(),
-		    buildConditions());
-	} else {
-	    throw new AttributeException(ARG_NOT_EXISTING);
-	}
-    }
-
-    protected String buildUrl() {
-	StringBuilder url = new StringBuilder();
-	url.append(JDBC_SERVICE).append(getServerAdress()).append(COLON).append(getPort())
-		.append(SLASH).append(getDb()).append(QM);
-
-	if (ssl) {
-	    url.append(SSL_PARAM);
-	}
-	if (urlParams != null && urlParams.length() > 0) {
-	    url.append(AMP).append(urlParams);
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(buildUrl(), getUsername(), getPassword());
 	}
 
-	return url.toString();
-    }
-
-    protected boolean verifySqlArguments() throws AttributeException {
-	try (Connection conn = getConnection()) {
-
-	    DatabaseMetaData dbm = conn.getMetaData();
-	    ResultSet cols = dbm.getColumns(null, null, getTable(), null);
-
-	    if (colsExist(cols, getIdColName(), getGeometryColName(), getPkColName())) {
-		return true;
-	    }
-	} catch (SQLException e) {
-	    throw new AttributeException(e);
-	}
-	return false;
-    }
-
-    private String buildConditions() {
-	StringBuilder conditions = new StringBuilder();
-	conditions.append(getPkColName()).append(GEQ).append(getFrom());
-	if (getUntil() >= getFrom()) {
-	    conditions.append(SQL_AND).append(getPkColName()).append(SEQ).append(getUntil());
-	}
-	return conditions.toString();
-    }
-
-    private String buildGeometryExpression() {
-	if (isEnableProjection() && getProjectionSRID() != 0) {
-	    return String.format(SQL_PROJECTGEOM, getGeometryColName(), getProjectionSRID());
-	} else {
-	    return String.format(SQL_GEOM, getGeometryColName());
-	}
-    }
-
-    protected static boolean colsExist(ResultSet cols, String... colNames) throws SQLException {
-	ArrayList<String> colNamesList = new ArrayList<>();
-
-	while (cols.next()) {
-	    colNamesList.add(cols.getString(COLUMN_NAME));
+	public String buildQuery() throws AttributeException {
+		if (verifySqlArguments()) {
+			return String.format(SQL_QUERY, getIdColName(), buildGeometryExpression(), getTable(), buildConditions());
+		} else {
+			throw new AttributeException(ARG_NOT_EXISTING);
+		}
 	}
 
-	for (String colName : colNames) {
-	    if (!colNamesList.contains(colName)) {
+	protected String buildUrl() {
+		StringBuilder url = new StringBuilder();
+		url.append(JDBC_SERVICE).append(getServerAdress()).append(COLON).append(getPort()).append(SLASH).append(getDb())
+				.append(QM);
+
+		if (ssl) {
+			url.append(SSL_PARAM);
+		}
+		if (urlParams != null && urlParams.length() > 0) {
+			url.append(AMP).append(urlParams);
+		}
+
+		return url.toString();
+	}
+
+	protected boolean verifySqlArguments() throws AttributeException {
+		try (Connection conn = getConnection()) {
+
+			DatabaseMetaData dbm = conn.getMetaData();
+			ResultSet cols = dbm.getColumns(null, null, getTable(), null);
+
+			if (colsExist(cols, getIdColName(), getGeometryColName(), getPkColName())) {
+				return true;
+			}
+		} catch (SQLException e) {
+			throw new AttributeException(e);
+		}
 		return false;
-	    }
 	}
-	return true;
 
-    }
+	private String buildConditions() {
+		StringBuilder conditions = new StringBuilder();
+		conditions.append(getPkColName()).append(GEQ).append(getFrom());
+		if (getUntil() >= getFrom()) {
+			conditions.append(SQL_AND).append(getPkColName()).append(SEQ).append(getUntil());
+		}
+		return conditions.toString();
+	}
+
+	private String buildGeometryExpression() {
+		if (isEnableProjection() && getProjectionSRID() != 0) {
+			return String.format(SQL_PROJECTGEOM, getGeometryColName(), getProjectionSRID());
+		} else {
+			return String.format(SQL_GEOM, getGeometryColName());
+		}
+	}
+
+	protected static boolean colsExist(ResultSet cols, String... colNames) throws SQLException {
+		ArrayList<String> colNamesList = new ArrayList<>();
+
+		while (cols.next()) {
+			colNamesList.add(cols.getString(COLUMN_NAME));
+		}
+
+		for (String colName : colNames) {
+			if (!colNamesList.contains(colName)) {
+				return false;
+			}
+		}
+		return true;
+
+	}
 }
--- sapl-geo/src/test/java/io/sapl/interpreter/pip/geo/PostGISTest.java
+++ sapl-geo/src/test/java/io/sapl/interpreter/pip/geo/PostGISTest.java
@@ -45,241 +45,208 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.sapl.api.functions.FunctionException;
 import io.sapl.api.pip.AttributeException;
-import io.sapl.interpreter.pip.geo.PostGISConfig;
-import io.sapl.interpreter.pip.geo.PostGISConnection;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
 public class PostGISTest {
-    private static final ObjectMapper MAPPER = new ObjectMapper();
-    private static final JsonNodeFactory JSON = JsonNodeFactory.instance;
-    private static final String JSON_CONF = "{\"serverAdress\": \"localhost\","
-	    + "\"port\": \"5432\","
-	    + "\"db\": \"db_sample\","
-	    + "\"table\": \"geofences\","
-	    + "\"username\": \"uname\","
-	    + "\"password\": \"pw\","
-	    + "\"geometryColName\": \"geom\","
-	    + "\"idColName\": \"fences\","
-	    + "\"pkColName\": \"gid\","
-	    + "\"from\": 0"
-	    + "%s}";
-    private static final String PROJECTION_CONFIG = ",\"enableProjection\": true,"
-	    + "\"projectionSRID\": 12345,"
-	    + "\"until\": 1";
-    private static final String JSON_ANSWER = "{\"name\":{\"type\":\"Point\",\"coordinates\":[1,1]}}";
-    private PostGISConfig pgConfFromJson;
-    private PostGISConfig pgProjectionConfFromJson;
-    private JsonNode jsonConf;
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+	private static final JsonNodeFactory JSON = JsonNodeFactory.instance;
+	private static final String JSON_CONF = "{\"serverAdress\": \"localhost\"," + "\"port\": \"5432\","
+			+ "\"db\": \"db_sample\"," + "\"table\": \"geofences\"," + "\"username\": \"uname\","
+			+ "\"password\": \"pw\"," + "\"geometryColName\": \"geom\"," + "\"idColName\": \"fences\","
+			+ "\"pkColName\": \"gid\"," + "\"from\": 0" + "%s}";
+	private static final String PROJECTION_CONFIG = ",\"enableProjection\": true," + "\"projectionSRID\": 12345,"
+			+ "\"until\": 1";
+	private static final String JSON_ANSWER = "{\"name\":{\"type\":\"Point\",\"coordinates\":[1,1]}}";
+	private PostGISConfig pgConfFromJson;
+	private PostGISConfig pgProjectionConfFromJson;
+	private JsonNode jsonConf;
 
-    @Before
-    public void init() throws IOException {
-	jsonConf = MAPPER.readValue(String.format(JSON_CONF, PROJECTION_CONFIG), JsonNode.class);
-	pgProjectionConfFromJson = MAPPER.convertValue(jsonConf, PostGISConfig.class);
+	@Before
+	public void init() throws IOException {
+		jsonConf = MAPPER.readValue(String.format(JSON_CONF, PROJECTION_CONFIG), JsonNode.class);
+		pgProjectionConfFromJson = MAPPER.convertValue(jsonConf, PostGISConfig.class);
 
-	jsonConf = MAPPER.readValue(String.format(JSON_CONF, ""), JsonNode.class);
-	pgConfFromJson = MAPPER.convertValue(jsonConf, PostGISConfig.class);
-    }
+		jsonConf = MAPPER.readValue(String.format(JSON_CONF, ""), JsonNode.class);
+		pgConfFromJson = MAPPER.convertValue(jsonConf, PostGISConfig.class);
+	}
 
-    @Test
-    public void builderConstructor() {
-	PostGISConfig conf = new PostGISConfig();
-	assertNotNull(
-		"PostGISConnection object fails to be constructed with empty PostGISConfig object.",
-		new PostGISConnection(conf));
-    }
+	@Test
+	public void builderConstructor() {
+		PostGISConfig conf = new PostGISConfig();
+		assertNotNull("PostGISConnection object fails to be constructed with empty PostGISConfig object.",
+				new PostGISConnection(conf));
+	}
 
-    @Test
-    public void jsonConstructor() {
-	PostGISConnection conn = new PostGISConnection(jsonConf);
-	assertEquals(
-		"PostGISConnection object fails to be constructed with proper JSON configuration.",
-		conn.getConfig(), pgConfFromJson);
-    }
+	@Test
+	public void jsonConstructor() {
+		PostGISConnection conn = new PostGISConnection(jsonConf);
+		assertEquals("PostGISConnection object fails to be constructed with proper JSON configuration.",
+				conn.getConfig(), pgConfFromJson);
+	}
 
-    @Test
-    public void confConstructor() {
-	PostGISConnection conn = new PostGISConnection(pgConfFromJson);
-	assertEquals(
-		"PostGISConnection object fails to be constructed with proper PostGISConfig object.",
-		conn.getConfig(), pgConfFromJson);
-    }
+	@Test
+	public void confConstructor() {
+		PostGISConnection conn = new PostGISConnection(pgConfFromJson);
+		assertEquals("PostGISConnection object fails to be constructed with proper PostGISConfig object.",
+				conn.getConfig(), pgConfFromJson);
+	}
 
-    @Test
-    public void equalsTest() {
-	EqualsVerifier.forClass(PostGISConfig.class)
-		.suppress(Warning.STRICT_INHERITANCE, Warning.NONFINAL_FIELDS).verify();
-    }
+	@Test
+	public void equalsTest() {
+		EqualsVerifier.forClass(PostGISConfig.class).suppress(Warning.STRICT_INHERITANCE, Warning.NONFINAL_FIELDS)
+				.verify();
+	}
 
-    @Test
-    public void responseTest() throws IOException, FunctionException, AttributeException {
-	PostGISConfig configMock = mock(PostGISConfig.class);
-	when(configMock.getTable()).thenReturn("testTable");
+	@Test
+	public void responseTest() throws IOException, FunctionException, AttributeException {
+		PostGISConfig configMock = mock(PostGISConfig.class);
+		when(configMock.getTable()).thenReturn("testTable");
 
-	PostGISConnection conn = new PostGISConnection(configMock);
-	PostGISConnection connSpy = spy(conn);
+		PostGISConnection conn = new PostGISConnection(configMock);
+		PostGISConnection connSpy = spy(conn);
 
-	ObjectNode response = JSON.objectNode();
-	JsonNode jsonPoint = MAPPER.readValue("{\"type\": \"Point\",\"coordinates\":[10.0, 15.0]}",
-		JsonNode.class);
-	response.set("testPoint", jsonPoint);
+		ObjectNode response = JSON.objectNode();
+		JsonNode jsonPoint = MAPPER.readValue("{\"type\": \"Point\",\"coordinates\":[10.0, 15.0]}", JsonNode.class);
+		response.set("testPoint", jsonPoint);
 
-	doReturn(response).when(connSpy).retrieveGeometries();
+		doReturn(response).when(connSpy).retrieveGeometries();
 
-	assertEquals("", connSpy.toGeoPIPResponse().toString(),
-		"{\"identifier\":\"testTable\",\"altitude\":0.0,\"accuracy\":0.0,\"trust\":0.0,"
-			+ "\"geofences\":{\"testPoint\":{\"type\":\"Point\",\"coordinates\":[10.0,15.0]}}}");
-    }
+		assertEquals("", connSpy.toGeoPIPResponse().toString(),
+				"{\"identifier\":\"testTable\",\"altitude\":0.0,\"accuracy\":0.0,\"trust\":0.0,"
+						+ "\"geofences\":{\"testPoint\":{\"type\":\"Point\",\"coordinates\":[10.0,15.0]}}}");
+	}
 
-    @Test
-    public void dbConnection()
-	    throws SQLException, FunctionException, AttributeException {
-	PostGISConfig configMock = mock(PostGISConfig.class);
-	Connection connMock = mock(Connection.class);
-	Statement sMock = mock(Statement.class);
-	ResultSet rsMock = mock(ResultSet.class);
+	@Test
+	public void dbConnection() throws SQLException, FunctionException, AttributeException {
+		PostGISConfig configMock = mock(PostGISConfig.class);
+		Connection connMock = mock(Connection.class);
+		Statement sMock = mock(Statement.class);
+		ResultSet rsMock = mock(ResultSet.class);
 
-	when(configMock.getConnection()).thenReturn(connMock);
-	when(configMock.buildQuery()).thenReturn("");
-	when(connMock.createStatement()).thenReturn(sMock);
-	when(sMock.executeQuery(anyString())).thenReturn(rsMock);
+		when(configMock.getConnection()).thenReturn(connMock);
+		when(configMock.buildQuery()).thenReturn("");
+		when(connMock.createStatement()).thenReturn(sMock);
+		when(sMock.executeQuery(anyString())).thenReturn(rsMock);
 
-	when(rsMock.next()).thenReturn(true).thenReturn(false);
-	when(rsMock.getString(eq(1))).thenReturn("name");
-	when(rsMock.getString(eq(2))).thenReturn("POINT (1 1)");
+		when(rsMock.next()).thenReturn(true).thenReturn(false);
+		when(rsMock.getString(eq(1))).thenReturn("name");
+		when(rsMock.getString(eq(2))).thenReturn("POINT (1 1)");
 
-	PostGISConnection c = new PostGISConnection(configMock);
-	assertEquals(
-		"Database connection is not correctly established or result not correctly formatted.",
-		JSON_ANSWER, c.retrieveGeometries().toString());
-    }
+		PostGISConnection c = new PostGISConnection(configMock);
+		assertEquals("Database connection is not correctly established or result not correctly formatted.", JSON_ANSWER,
+				c.retrieveGeometries().toString());
+	}
 
-    @Test
-    public void buildQuery() throws AttributeException {
-	PostGISConfig pgConfSpy = spy(pgConfFromJson);
-	doReturn(true).when(pgConfSpy).verifySqlArguments();
+	@Test
+	public void buildQuery() throws AttributeException {
+		PostGISConfig pgConfSpy = spy(pgConfFromJson);
+		doReturn(true).when(pgConfSpy).verifySqlArguments();
 
-	assertEquals("PostGIS SQL Query is not correctly build.", pgConfSpy.buildQuery(),
-		"SELECT fences, ST_AsText(geom) FROM geofences WHERE gid>=0;");
-    }
+		assertEquals("PostGIS SQL Query is not correctly build.", pgConfSpy.buildQuery(),
+				"SELECT fences, ST_AsText(ST_FlipCoordinates(geom)) FROM geofences WHERE gid>=0;");
+	}
 
-    @Test(expected = AttributeException.class)
-    public void buildQueryException() throws AttributeException {
-	PostGISConfig pgConfSpy = spy(pgConfFromJson);
-	doReturn(false).when(pgConfSpy).verifySqlArguments();
+	@Test(expected = AttributeException.class)
+	public void buildQueryException() throws AttributeException {
+		PostGISConfig pgConfSpy = spy(pgConfFromJson);
+		doReturn(false).when(pgConfSpy).verifySqlArguments();
 
-	assertNotEquals("PostGIS SQL Query is build even though parameters are not validated.",
-		pgConfSpy.buildQuery(),
-		"SELECT fences, ST_AsText(geom) FROM geofences WHERE gid>=0;");
-    }
+		assertNotEquals("PostGIS SQL Query is build even though parameters are not validated.", pgConfSpy.buildQuery(),
+				"SELECT fences, ST_AsText(ST_FlipCoordinates(geom)) FROM geofences WHERE gid>=0;");
+	}
 
-    @Test
-    public void buildProjectionQuery() throws AttributeException {
-	PostGISConfig pgConfSpy = spy(pgProjectionConfFromJson);
-	doReturn(true).when(pgConfSpy).verifySqlArguments();
+	@Test
+	public void buildProjectionQuery() throws AttributeException {
+		PostGISConfig pgConfSpy = spy(pgProjectionConfFromJson);
+		doReturn(true).when(pgConfSpy).verifySqlArguments();
+		assertEquals("PostGIS SQL Query is not correctly build.", pgConfSpy.buildQuery(),
+				"SELECT fences, ST_AsText(ST_FlipCoordinates(ST_Transform(geom,12345))) FROM geofences WHERE gid>=0 AND gid<=1;");
+	}
 
-	assertEquals("PostGIS SQL Query is not correctly build.", pgConfSpy.buildQuery(),
-		"SELECT fences, ST_AsText(ST_Transform(geom,12345)) FROM geofences WHERE gid>=0 AND gid<=1;");
-    }
+	@Test(expected = AttributeException.class)
+	public void dbException() throws SQLException, FunctionException, AttributeException {
+		PostGISConfig configMock = mock(PostGISConfig.class);
+		when(configMock.getConnection()).thenThrow(new SQLException());
 
-    @Test(expected = AttributeException.class)
-    public void dbException()
-	    throws SQLException, FunctionException, AttributeException {
-	PostGISConfig configMock = mock(PostGISConfig.class);
-	when(configMock.getConnection()).thenThrow(new SQLException());
+		PostGISConnection c = new PostGISConnection(configMock);
+		assertTrue("Handling of SQLException in PostGISConnection works not as expected.",
+				c.retrieveGeometries().isNull());
+	}
 
-	PostGISConnection c = new PostGISConnection(configMock);
-	assertTrue("Handling of SQLException in PostGISConnection works not as expected.",
-		c.retrieveGeometries().isNull());
-    }
+	@Test
+	public void colsExistTrue() throws SQLException {
+		ResultSet rsMock = mock(ResultSet.class);
+		when(rsMock.next()).thenReturn(true).thenReturn(false);
+		when(rsMock.getString(anyString())).thenReturn("test");
 
-    @Test
-    public void colsExistTrue() throws SQLException {
-	ResultSet rsMock = mock(ResultSet.class);
-	when(rsMock.next()).thenReturn(true).thenReturn(false);
-	when(rsMock.getString(anyString())).thenReturn("test");
+		assertTrue("colsExist() returns false even though column title exists in ResultSet.",
+				PostGISConfig.colsExist(rsMock, "test"));
+	}
 
-	assertTrue("colsExist() returns false even though column title exists in ResultSet.",
-		PostGISConfig.colsExist(rsMock, "test"));
-    }
+	@Test
+	public void colsExistFalse() throws SQLException {
+		ResultSet rsMock = mock(ResultSet.class);
+		when(rsMock.next()).thenReturn(false);
 
-    @Test
-    public void colsExistFalse() throws SQLException {
-	ResultSet rsMock = mock(ResultSet.class);
-	when(rsMock.next()).thenReturn(false);
+		assertFalse("colsExist() returns true even though column title does not exist in ResultSet.",
+				PostGISConfig.colsExist(rsMock, "test"));
+	}
 
-	assertFalse(
-		"colsExist() returns true even though column title does not exist in ResultSet.",
-		PostGISConfig.colsExist(rsMock, "test"));
-    }
+	@Test(expected = AttributeException.class)
+	public void sqlException() throws AttributeException, SQLException {
+		PostGISConfig pgConfSpy = spy(pgProjectionConfFromJson);
+		doThrow(new SQLException()).when(pgConfSpy).getConnection();
 
-    @Test(expected = AttributeException.class)
-    public void sqlException() throws AttributeException, SQLException {
-	PostGISConfig pgConfSpy = spy(pgProjectionConfFromJson);
-	doThrow(new SQLException()).when(pgConfSpy).getConnection();
+		assertTrue("SQLException is not correctly handled.", pgConfSpy.verifySqlArguments());
+	}
 
-	assertTrue("SQLException is not correctly handled.", pgConfSpy.verifySqlArguments());
-    }
+	@Test
+	public void verifySqlTrue() throws AttributeException, SQLException {
+		PostGISConfig pgConfSpy = spy(pgConfFromJson);
+		Connection connMock = mock(Connection.class);
+		ResultSet rsMock = mock(ResultSet.class);
+		DatabaseMetaData dbmMock = mock(DatabaseMetaData.class);
 
-    @Test
-    public void verifySqlTrue() throws AttributeException, SQLException {
-	PostGISConfig pgConfSpy = spy(pgConfFromJson);
-	Connection connMock = mock(Connection.class);
-	ResultSet rsMock = mock(ResultSet.class);
-	DatabaseMetaData dbmMock = mock(DatabaseMetaData.class);
+		doReturn(connMock).when(pgConfSpy).getConnection();
+		when(connMock.getMetaData()).thenReturn(dbmMock);
+		when(dbmMock.getColumns(eq(null), eq(null), anyString(), eq(null))).thenReturn(rsMock);
+		when(rsMock.next()).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
+		when(rsMock.getString(anyString())).thenReturn("fences").thenReturn("gid").thenReturn("geom");
 
-	doReturn(connMock).when(pgConfSpy).getConnection();
-	when(connMock.getMetaData()).thenReturn(dbmMock);
-	when(dbmMock.getColumns(eq(null), eq(null), anyString(), eq(null)))
-		.thenReturn(rsMock);
-	when(rsMock.next()).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
-	when(rsMock.getString(anyString())).thenReturn("fences").thenReturn("gid")
-		.thenReturn("geom");
+		assertTrue("Arguments for SQL query are not correctly validated.", pgConfSpy.verifySqlArguments());
+	}
 
-	assertTrue("Arguments for SQL query are not correctly validated.",
-		pgConfSpy.verifySqlArguments());
-    }
+	@Test
+	public void verifySqlFalse() throws AttributeException, SQLException {
+		PostGISConfig pgConfSpy = spy(pgConfFromJson);
+		Connection connMock = mock(Connection.class);
+		ResultSet rsMock = mock(ResultSet.class);
+		DatabaseMetaData dbmMock = mock(DatabaseMetaData.class);
 
-    @Test
-    public void verifySqlFalse() throws AttributeException, SQLException {
-	PostGISConfig pgConfSpy = spy(pgConfFromJson);
-	Connection connMock = mock(Connection.class);
-	ResultSet rsMock = mock(ResultSet.class);
-	DatabaseMetaData dbmMock = mock(DatabaseMetaData.class);
+		doReturn(connMock).when(pgConfSpy).getConnection();
+		when(connMock.getMetaData()).thenReturn(dbmMock);
+		when(dbmMock.getColumns(eq(null), eq(null), anyString(), eq(null))).thenReturn(rsMock);
+		when(rsMock.next()).thenReturn(true).thenReturn(false);
+		when(rsMock.getString(anyString())).thenReturn("fences");
 
-	doReturn(connMock).when(pgConfSpy).getConnection();
-	when(connMock.getMetaData()).thenReturn(dbmMock);
-	when(dbmMock.getColumns(eq(null), eq(null), anyString(), eq(null)))
-		.thenReturn(rsMock);
-	when(rsMock.next()).thenReturn(true).thenReturn(false);
-	when(rsMock.getString(anyString())).thenReturn("fences");
+		assertFalse("Arguments for SQL query are not correctly validated.", pgConfSpy.verifySqlArguments());
+	}
 
-	assertFalse("Arguments for SQL query are not correctly validated.",
-		pgConfSpy.verifySqlArguments());
-    }
+	@Test
+	public void buildUrl() {
+		assertEquals("PostGIS-URL is not correctly built.", pgConfFromJson.buildUrl(),
+				"jdbc:postgresql://localhost:5432/db_sample?");
+	}
 
-    @Test
-    public void buildUrl() {
-	assertEquals("PostGIS-URL is not correctly built.", pgConfFromJson.buildUrl(),
-		"jdbc:postgresql://localhost:5432/db_sample?");
-    }
-
-    @Test
-    public void buildUrlWithParams() throws IOException {
-	String config = "{\"serverAdress\": \"localhost\","
-		+ "\"port\": \"5432\","
-		+ "\"db\": \"db_sample\","
-		+ "\"table\": \"geofences\","
-		+ "\"username\": \"uname\","
-		+ "\"password\": \"pw\","
-		+ "\"geometryColName\": \"geom\","
-		+ "\"idColName\": \"fences\","
-		+ "\"pkColName\": \"gid\","
-		+ "\"from\": 0,"
-		+ "\"ssl\": true,"
-		+ "\"urlParams\": \"test=test\"}";
-	PostGISConfig pgConfWithParams = MAPPER.readValue(config, PostGISConfig.class);
-	assertEquals("PostGIS-URL is not correctly built.", pgConfWithParams.buildUrl(),
-		"jdbc:postgresql://localhost:5432/db_sample?ssl=true&test=test");
-    }
+	@Test
+	public void buildUrlWithParams() throws IOException {
+		String config = "{\"serverAdress\": \"localhost\"," + "\"port\": \"5432\"," + "\"db\": \"db_sample\","
+				+ "\"table\": \"geofences\"," + "\"username\": \"uname\"," + "\"password\": \"pw\","
+				+ "\"geometryColName\": \"geom\"," + "\"idColName\": \"fences\"," + "\"pkColName\": \"gid\","
+				+ "\"from\": 0," + "\"ssl\": true," + "\"urlParams\": \"test=test\"}";
+		PostGISConfig pgConfWithParams = MAPPER.readValue(config, PostGISConfig.class);
+		assertEquals("PostGIS-URL is not correctly built.", pgConfWithParams.buildUrl(),
+				"jdbc:postgresql://localhost:5432/db_sample?ssl=true&test=test");
+	}
 }
