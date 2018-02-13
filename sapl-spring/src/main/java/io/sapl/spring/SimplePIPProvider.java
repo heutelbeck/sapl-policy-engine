3be1bf849529c750f2f6e201e32173abe562b692
Benedikt Heinrich <benedikt.heinrich@web.de>
2018-02-13 20:09:07 +0100

    sonarqube issues

--- sapl-spring/src/main/java/io/sapl/spring/SimplePIPProvider.java
+++ sapl-spring/src/main/java/io/sapl/spring/SimplePIPProvider.java
@@ -1,14 +1,16 @@
 package io.sapl.spring;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class SimplePIPProvider implements PIPProvider {
 	
+	private Collection<Class<? extends Object>> pipList;
+	
 	public SimplePIPProvider (Collection<Class<? extends Object>> pipList) {
-		this.pipList = pipList;
+		this.pipList = Collections.unmodifiableCollection(pipList);
 	}
 	
-	private Collection<Class<? extends Object>> pipList;
 	
 	@Override
 	public Collection<Class<? extends Object>> getPIPClasses() {
