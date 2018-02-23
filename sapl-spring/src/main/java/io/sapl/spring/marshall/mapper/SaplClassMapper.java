f48a6f526b9410595f03de2c59b0eddfe09ad624
Benedikt Heinrich <benedikt.heinrich@web.de>
2018-02-23 01:03:08 +0100

    more work on saplMapper

--- sapl-spring/src/main/java/io/sapl/spring/marshall/mapper/SaplClassMapper.java
+++ sapl-spring/src/main/java/io/sapl/spring/marshall/mapper/SaplClassMapper.java
@@ -1,10 +1,36 @@
 package io.sapl.spring.marshall.mapper;
 
+
+/**
+ * Each SaplClassMapper can map Objects of a certain class.
+ * The SaplClassMappers should be registered to the SaplMapper.
+ */
 public interface SaplClassMapper {
 
-	
+	/**
+	 * Receives an Object and maps it to something else.
+	 * @param objectToMap The object that should be mapped.
+	 * @return The mapped Object.
+	 */
 	Object map (Object objectToMap);
 	
-	boolean canMap(Object objectToMap);
+	
+	/**
+	 * Checks if an Object can be mapped by this mapper.
+	 * @param objectToMap The object that should be mapped.
+	 * @return True, if the Object can be mapped, or false if not.
+	 */
+	default boolean canMap(Object objectToMap) {
+		if (objectToMap.getClass().toString().equals(getMappedClass())) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Provides the class this mapper can map.
+	 * @return The String name of the class that this Mapper can map.
+	 */
+	String getMappedClass();
 
 }
--- sapl-spring/src/main/java/io/sapl/spring/marshall/mapper/SimpleSaplMapper.java
+++ sapl-spring/src/main/java/io/sapl/spring/marshall/mapper/SimpleSaplMapper.java
@@ -2,7 +2,6 @@ package io.sapl.spring.marshall.mapper;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 
 
