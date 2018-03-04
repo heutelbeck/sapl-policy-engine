3dea0b1a6422ba9c09c909e6abdbb400cbcde33a
Miro Selent <miros@gmx.de>
2018-03-04 20:54:27 +0100

    Added additional validation support and fixed some code generation issues.

--- sapl-web/src/main/java/io/sapl/grammar/web/XtextServletConfiguration.java
+++ sapl-web/src/main/java/io/sapl/grammar/web/XtextServletConfiguration.java
@@ -1,5 +1,6 @@
 package io.sapl.grammar.web;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.filter.OrderedHttpPutFormContentFilter;
@@ -7,30 +8,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+@EnableAutoConfiguration
 @Configuration
 public class XtextServletConfiguration {
-	@Bean
-	public static ServletRegistrationBean<SAPLServlet> xTextRegistrationBean() {
-		ServletRegistrationBean<SAPLServlet> registration = new ServletRegistrationBean<>(new SAPLServlet(),
-				"/xtext-service/*");
-		registration.setName("XtextServices");
-		registration.setAsyncSupported(true);
-		return registration;
-	}
+    @Bean
+    public static ServletRegistrationBean<SAPLServlet> xTextRegistrationBean() {
+	ServletRegistrationBean<SAPLServlet> registration = new ServletRegistrationBean<>(new SAPLServlet(),
+		"/xtext-service/*");
+	registration.setName("XtextServices");
+	registration.setAsyncSupported(true);
+	return registration;
+    }
 
-	@Bean
-	public static FilterRegistrationBean<OrderedHttpPutFormContentFilter> registration1(
-			OrderedHttpPutFormContentFilter filter) {
-		FilterRegistrationBean<OrderedHttpPutFormContentFilter> registration = new FilterRegistrationBean<>(filter);
-		registration.setEnabled(false);
-		return registration;
-	}
+    @Bean
+    public static FilterRegistrationBean<OrderedHttpPutFormContentFilter> registration1(
+	    OrderedHttpPutFormContentFilter filter) {
+	FilterRegistrationBean<OrderedHttpPutFormContentFilter> registration = new FilterRegistrationBean<>(filter);
+	registration.setEnabled(false);
+	return registration;
+    }
 
-	@Bean
-	public static FilterRegistrationBean<HiddenHttpMethodFilter> registration2(HiddenHttpMethodFilter filter) {
-		FilterRegistrationBean<HiddenHttpMethodFilter> registration = new FilterRegistrationBean<>(filter);
-		registration.setEnabled(false);
-		return registration;
-	}
+    @Bean
+    public static FilterRegistrationBean<HiddenHttpMethodFilter> registration2(HiddenHttpMethodFilter filter) {
+	FilterRegistrationBean<HiddenHttpMethodFilter> registration = new FilterRegistrationBean<>(filter);
+	registration.setEnabled(false);
+	return registration;
+    }
 
 }
