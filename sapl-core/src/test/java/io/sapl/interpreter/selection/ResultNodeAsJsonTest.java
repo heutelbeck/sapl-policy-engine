d9d6961f7c0709ff34470427eca1e33596cefcad
Felix Siegrist <felix.siegrist@inventage.com>
2018-12-23 20:49:11 +0100

    Add unit tests for reactive ResultNode methods

---
+++ sapl-core/src/test/java/io/sapl/interpreter/selection/ResultNodeAsJsonTest.java
@@ -0,0 +1,57 @@
+package io.sapl.interpreter.selection;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import org.junit.Test;
+
+public class ResultNodeAsJsonTest {
+	private static JsonNodeFactory JSON = JsonNodeFactory.instance;
+
+	@Test
+	public void asJsonWithoutParent() {
+		ResultNode resultNode = new JsonNodeWithoutParent(JSON.nullNode());
+		JsonNode expectedResult = JSON.nullNode();
+
+		assertEquals("asJson method JsonNodeWithoutParent should return contained JSON node", expectedResult,
+				resultNode.asJsonWithoutAnnotations());
+	}
+
+	@Test
+	public void asJsonWithParentObject() {
+		ResultNode resultNode = new JsonNodeWithParentObject(JSON.nullNode(), JSON.objectNode(), "key");
+		JsonNode expectedResult = JSON.nullNode();
+
+		assertEquals("asJson method JsonNodeWithParentObject should return contained JSON node", expectedResult,
+				resultNode.asJsonWithoutAnnotations());
+	}
+
+	@Test
+	public void asJsonWithParentArray() {
+		ResultNode resultNode = new JsonNodeWithParentArray(JSON.nullNode(), JSON.arrayNode(), 0);
+		JsonNode expectedResult = JSON.nullNode();
+
+		assertEquals("asJson method JsonNodeWithParentArray should return contained JSON node", expectedResult,
+				resultNode.asJsonWithoutAnnotations());
+	}
+
+	@Test
+	public void asJsonArrayResultNode() {
+		List<AbstractAnnotatedJsonNode> list = new ArrayList<>();
+		list.add(new JsonNodeWithoutParent(JSON.nullNode()));
+		list.add(new JsonNodeWithoutParent(JSON.booleanNode(true)));
+		ArrayResultNode resultNode = new ArrayResultNode(list);
+
+		ArrayNode expectedResult = JSON.arrayNode();
+		expectedResult.add(JSON.nullNode());
+		expectedResult.add(JSON.booleanNode(true));
+
+		assertEquals("asJson method ArrayResultNode should return array node containing the JSON nodes", expectedResult,
+				resultNode.asJsonWithoutAnnotations());
+	}
+}
