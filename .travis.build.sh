e63bc5e3e23bdefd50cbe6fe85c3edf2e653d166
heutelbeck <dheutelbeck@ftk.de>
2019-02-09 22:44:53 +0100

    setting travis build to batch mode

--- .travis.build.sh
+++ .travis.build.sh
@@ -9,13 +9,13 @@ fi
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
     if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
         echo "Building master"
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=http://sonar.ftk.de:9000  -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/*
-        mvn deploy -DskipTests=true -Dmaven.javadoc.skip=true
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=http://sonar.ftk.de:9000  -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/* --batch-mode
+        mvn deploy -DskipTests=true -Dmaven.javadoc.skip=true --batch-mode
     else
         echo "Building pull request"
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --batch-mode
     fi
 else
     echo "Building branch ${TRAVIS_BRANCH}"
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --batch-mode
 fi
