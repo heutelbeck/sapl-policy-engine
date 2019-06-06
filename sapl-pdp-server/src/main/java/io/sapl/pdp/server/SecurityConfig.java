b18709f0e4c5a359959fda8ef0c123d2675ea268
Felix Siegrist <felix.siegrist@inventage.com>
2019-06-06 21:42:01 +0200

    Set bcrypt encoded client-secret for PDP server on in memory authentication manager

--- sapl-pdp-server/src/main/java/io/sapl/pdp/server/SecurityConfig.java
+++ sapl-pdp-server/src/main/java/io/sapl/pdp/server/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 
 }
