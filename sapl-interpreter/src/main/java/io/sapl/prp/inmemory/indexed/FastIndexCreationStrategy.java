51216b97a0b42d170ffe7f9ef985ef63e9be4abd
heutelbeck <dheutelbeck@ftk.de>
2019-06-11 17:11:39 +0200

    sonarqube issue

--- sapl-interpreter/src/main/java/io/sapl/prp/inmemory/indexed/FastIndexCreationStrategy.java
+++ sapl-interpreter/src/main/java/io/sapl/prp/inmemory/indexed/FastIndexCreationStrategy.java
@@ -18,12 +18,10 @@ import io.sapl.grammar.sapl.SAPL;
 public class FastIndexCreationStrategy implements IndexCreationStrategy {
 
 	@Override
-	public IndexContainer construct(final Map<String, SAPL> documents,
-			final Map<String, DisjunctiveFormula> targets) {
+	public IndexContainer construct(final Map<String, SAPL> documents, final Map<String, DisjunctiveFormula> targets) {
 		Map<String, SAPL> idToDocument = ImmutableMap.copyOf(documents);
 
-		Map<DisjunctiveFormula, Set<SAPL>> formulaToDocuments = mapFormulaToDocuments(
-				targets, idToDocument);
+		Map<DisjunctiveFormula, Set<SAPL>> formulaToDocuments = mapFormulaToDocuments(targets, idToDocument);
 
 		Map<ConjunctiveClause, Set<DisjunctiveFormula>> clauseToFormulas = mapClauseToFormulas(
 				formulaToDocuments.keySet());
@@ -32,24 +30,20 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 		List<Variable> variableOrder = createVariableOrder(data);
 		BiMap<ConjunctiveClause, Integer> clauseToIndexBiMap = createCandidateOrder(data);
 
-		Map<Integer, Set<DisjunctiveFormula>> indexToTargets = mapIndexToTargets(
-				clauseToIndexBiMap, clauseToFormulas);
+		Map<Integer, Set<DisjunctiveFormula>> indexToTargets = mapIndexToTargets(clauseToIndexBiMap, clauseToFormulas);
 
-		Map<DisjunctiveFormula, Bitmask> formulaToClauses = mapFormulaToClauses(
-				formulaToDocuments.keySet(), clauseToIndexBiMap);
+		Map<DisjunctiveFormula, Bitmask> formulaToClauses = mapFormulaToClauses(formulaToDocuments.keySet(),
+				clauseToIndexBiMap);
 
 		int[] indexToLengthTemplate = mapIndexToLength(clauseToIndexBiMap.inverse());
-		int[] indexToOccurencesTemplate = mapIndexToOccurences(
-				clauseToIndexBiMap.inverse(), clauseToFormulas);
-		AuxiliaryMatrix matrix = new AuxiliaryMatrix(indexToLengthTemplate,
-				indexToOccurencesTemplate);
+		int[] indexToOccurencesTemplate = mapIndexToOccurences(clauseToIndexBiMap.inverse(), clauseToFormulas);
+		AuxiliaryMatrix matrix = new AuxiliaryMatrix(indexToLengthTemplate, indexToOccurencesTemplate);
 
-		return new FastIndexContainer(true, variableOrder, formulaToClauses,
-				flattenIndexMap(indexToTargets), formulaToDocuments, matrix);
+		return new FastIndexContainer(true, variableOrder, formulaToClauses, flattenIndexMap(indexToTargets),
+				formulaToDocuments, matrix);
 	}
 
-	private static Collection<VariableInfo> collectData(
-			final Collection<DisjunctiveFormula> formulas) {
+	private static Collection<VariableInfo> collectData(final Collection<DisjunctiveFormula> formulas) {
 		Map<Bool, VariableInfo> boolToVariableInfo = new HashMap<>();
 		Set<Bool> negativesGroupedByFormula = new HashSet<>();
 		Set<Bool> positivesGroupedByFormula = new HashSet<>();
@@ -60,26 +54,21 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 				List<Literal> literals = clause.getLiterals();
 				final int sizeOfClause = literals.size();
 				for (Literal literal : literals) {
-					collectDataImpl(literal, clause, boolToVariableInfo,
-							negativesGroupedByFormula, positivesGroupedByFormula,
-							sizeOfClause);
+					collectDataImpl(literal, clause, boolToVariableInfo, negativesGroupedByFormula,
+							positivesGroupedByFormula, sizeOfClause);
 				}
 			}
 		}
 		for (VariableInfo variableInfo : boolToVariableInfo.values()) {
-			double sum = variableInfo.getClauseRelevancesList().stream()
-					.mapToDouble(x -> x).sum();
-			sum /= (variableInfo.getNumberOfPositives()
-					+ variableInfo.getNumberOfNegatives());
+			double sum = variableInfo.getClauseRelevancesList().stream().mapToDouble(Double::new).sum();
+			sum /= variableInfo.getNumberOfPositives() + variableInfo.getNumberOfNegatives();
 			variableInfo.setRelevance(sum);
 		}
 		return boolToVariableInfo.values();
 	}
 
-	private static void collectDataImpl(final Literal literal,
-			final ConjunctiveClause clause,
-			final Map<Bool, VariableInfo> boolToVariableInfo,
-			final Set<Bool> negativesGroupedByFormula,
+	private static void collectDataImpl(final Literal literal, final ConjunctiveClause clause,
+			final Map<Bool, VariableInfo> boolToVariableInfo, final Set<Bool> negativesGroupedByFormula,
 			final Set<Bool> positivesGroupedByFormula, final int sizeOfClause) {
 		Bool bool = literal.getBool();
 		VariableInfo variableInfo = boolToVariableInfo.get(bool);
@@ -107,14 +96,12 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 		}
 	}
 
-	private static BiMap<ConjunctiveClause, Integer> createCandidateOrder(
-			final Collection<VariableInfo> data) {
+	private static BiMap<ConjunctiveClause, Integer> createCandidateOrder(final Collection<VariableInfo> data) {
 		BiMap<ConjunctiveClause, Integer> result = HashBiMap.create();
 		int i = 0;
 		for (VariableInfo variableInfo : data) {
 			Variable variable = variableInfo.getVariable();
-			for (ConjunctiveClause clause : variableInfo
-					.getSetOfUnsatisfiableClausesIfTrue()) {
+			for (ConjunctiveClause clause : variableInfo.getSetOfUnsatisfiableClausesIfTrue()) {
 				Integer index = result.get(clause);
 				if (index == null) {
 					index = i;
@@ -124,8 +111,7 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 				variable.getUnsatisfiedCandidatesWhenTrue().set(index);
 				variable.getCandidates().set(index);
 			}
-			for (ConjunctiveClause clause : variableInfo
-					.getSetOfUnsatisfiableClausesIfFalse()) {
+			for (ConjunctiveClause clause : variableInfo.getSetOfUnsatisfiableClausesIfFalse()) {
 				Integer index = result.get(clause);
 				if (index == null) {
 					index = i;
@@ -146,14 +132,11 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 		final double relevance = variableInfo.getRelevance();
 		final double costs = 1.0;
 
-		return ((Math.pow(relevance, square - relevance)
-				* (groupedPositives + groupedNegatives)) / costs)
-				* (square - Math.pow((groupedPositives - groupedNegatives)
-						/ (groupedPositives + groupedNegatives), square));
+		return Math.pow(relevance, square - relevance) * (groupedPositives + groupedNegatives) / costs * (square
+				- Math.pow((groupedPositives - groupedNegatives) / (groupedPositives + groupedNegatives), square));
 	}
 
-	private static List<Variable> createVariableOrder(
-			final Collection<VariableInfo> data) {
+	private static List<Variable> createVariableOrder(final Collection<VariableInfo> data) {
 		List<VariableInfo> infos = new ArrayList<>(data);
 		for (VariableInfo variableInfo : data) {
 			variableInfo.setEnergyScore(createScore(variableInfo));
@@ -188,8 +171,7 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 		return result;
 	}
 
-	private static Map<DisjunctiveFormula, Bitmask> mapFormulaToClauses(
-			final Collection<DisjunctiveFormula> formulas,
+	private static Map<DisjunctiveFormula, Bitmask> mapFormulaToClauses(final Collection<DisjunctiveFormula> formulas,
 			final Map<ConjunctiveClause, Integer> clauseToIndex) {
 		final Map<DisjunctiveFormula, Bitmask> result = new HashMap<>();
 		for (DisjunctiveFormula formula : formulas) {
@@ -206,8 +188,7 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 	}
 
 	private static Map<DisjunctiveFormula, Set<SAPL>> mapFormulaToDocuments(
-			final Map<String, DisjunctiveFormula> targets,
-			final Map<String, SAPL> documents) {
+			final Map<String, DisjunctiveFormula> targets, final Map<String, SAPL> documents) {
 		Map<DisjunctiveFormula, Set<SAPL>> result = new HashMap<>();
 		for (Map.Entry<String, DisjunctiveFormula> entry : targets.entrySet()) {
 			DisjunctiveFormula formula = entry.getValue();
@@ -221,29 +202,24 @@ public class FastIndexCreationStrategy implements IndexCreationStrategy {
 		return result;
 	}
 
-	private static int[] mapIndexToLength(
-			final Map<Integer, ConjunctiveClause> indexToClause) {
+	private static int[] mapIndexToLength(final Map<Integer, ConjunctiveClause> indexToClause) {
 		final int[] result = new int[indexToClause.size()];
 		indexToClause.forEach((key, value) -> result[key] = value.size());
 		return result;
 	}
 
-	private static int[] mapIndexToOccurences(
-			final Map<Integer, ConjunctiveClause> indexToClause,
+	private static int[] mapIndexToOccurences(final Map<Integer, ConjunctiveClause> indexToClause,
 			final Map<ConjunctiveClause, Set<DisjunctiveFormula>> clauseToFormulas) {
 		final int[] result = new int[indexToClause.size()];
-		indexToClause.forEach(
-				(key, value) -> result[key] = clauseToFormulas.get(value).size());
+		indexToClause.forEach((key, value) -> result[key] = clauseToFormulas.get(value).size());
 		return result;
 	}
 
 	private static Map<Integer, Set<DisjunctiveFormula>> mapIndexToTargets(
 			final Map<ConjunctiveClause, Integer> clauseToIndex,
 			final Map<ConjunctiveClause, Set<DisjunctiveFormula>> clauseToFormulas) {
-		Map<Integer, Set<DisjunctiveFormula>> result = new HashMap<>(
-				clauseToIndex.size());
-		for (Map.Entry<ConjunctiveClause, Set<DisjunctiveFormula>> entry : clauseToFormulas
-				.entrySet()) {
+		Map<Integer, Set<DisjunctiveFormula>> result = new HashMap<>(clauseToIndex.size());
+		for (Map.Entry<ConjunctiveClause, Set<DisjunctiveFormula>> entry : clauseToFormulas.entrySet()) {
 			result.put(clauseToIndex.get(entry.getKey()), entry.getValue());
 		}
 		return result;
