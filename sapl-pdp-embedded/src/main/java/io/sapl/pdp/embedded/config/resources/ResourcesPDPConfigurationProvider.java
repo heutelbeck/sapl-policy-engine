c4ab5b1c36fc9d4c8f80212ec880ae317d5458d0
Felix Siegrist <felix.siegrist@inventage.com>
2019-10-16 18:08:24 +0200

    Fix access to config and policy files in jar archive

--- sapl-pdp-embedded/src/main/java/io/sapl/pdp/embedded/config/resources/ResourcesPDPConfigurationProvider.java
+++ sapl-pdp-embedded/src/main/java/io/sapl/pdp/embedded/config/resources/ResourcesPDPConfigurationProvider.java
@@ -1,17 +1,21 @@
 package io.sapl.pdp.embedded.config.resources;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.IOUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -47,38 +51,65 @@ public class ResourcesPDPConfigurationProvider implements PDPConfigurationProvid
 	public ResourcesPDPConfigurationProvider(@NonNull Class<?> clazz, @NonNull String configPath)
 			throws PDPConfigurationException, IOException, URISyntaxException {
 		URL configFolderUrl = clazz.getResource(configPath);
-
 		if (configFolderUrl == null) {
 			throw new PDPConfigurationException("Config folder not found. Path:" + configPath + " - URL: null");
 		}
 
-		Path path;
-		FileSystem fs = null;
+		if ("jar".equals(configFolderUrl.getProtocol())) {
+			readConfigFromJar(configFolderUrl);
+		}
+		else {
+			readConfigFromDirectory(configFolderUrl);
+		}
+
+		if (this.config == null) {
+			LOGGER.debug("config is null - using default config");
+			this.config = new PolicyDecisionPointConfiguration();
+		}
+	}
+
+	private void readConfigFromJar(URL configFolderUrl) {
+		LOGGER.debug("reading config from jar {}", configFolderUrl);
+		final String[] jarPathElements = configFolderUrl.toString().split("!");
+		final String jarFilePath = jarPathElements[0].substring("jar:file:".length());
+		String dirPath = "";
+		for (int i = 1; i < jarPathElements.length; i++) {
+			dirPath += jarPathElements[i];
+		}
+		if (dirPath.startsWith(File.separator)) {
+			dirPath = dirPath.substring(1);
+		}
+		final String configFilePath = dirPath + File.separator + CONFIG_FILE_GLOB_PATTERN;
+
 		try {
-			if ("jar".equals(configFolderUrl.getProtocol())) {
-				final Map<String, String> env = new HashMap<>();
-				final String[] array = configFolderUrl.toString().split("!");
-				fs = FileSystems.newFileSystem(URI.create(array[0]), env);
-				path = fs.getPath(array[1]);
-			}
-			else {
-				path = Paths.get(configFolderUrl.toURI());
-			}
-			LOGGER.info("current path: {}", path);
-			try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, CONFIG_FILE_GLOB_PATTERN)) {
-				for (Path filePath : stream) {
-					LOGGER.info("load: {}", filePath);
-					this.config = MAPPER.readValue(filePath.toFile(), PolicyDecisionPointConfiguration.class);
+			ZipFile zipFile = new ZipFile(jarFilePath);
+			Enumeration<? extends ZipEntry> e = zipFile.entries();
+
+			while (e.hasMoreElements()) {
+				ZipEntry entry = e.nextElement();
+				if (!entry.isDirectory() && entry.getName().equals(configFilePath)) {
+					LOGGER.info("load: {}", entry.getName());
+					BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));
+					String fileContentsStr = IOUtils.toString(bis, StandardCharsets.UTF_8);
+					bis.close();
+					this.config = MAPPER.readValue(fileContentsStr, PolicyDecisionPointConfiguration.class);
 					break;
-				}
-				if (this.config == null) {
-					this.config = new PolicyDecisionPointConfiguration();
 				}
 			}
 		}
-		finally {
-			if (fs != null) {
-				fs.close();
+		catch (IOException e) {
+			LOGGER.error("Error while reading config from jar", e);
+		}
+	}
+
+	private void readConfigFromDirectory(URL configFolderUrl) throws IOException, URISyntaxException {
+		LOGGER.debug("reading config from directory {}", configFolderUrl);
+		Path configDirectoryPath = Paths.get(configFolderUrl.toURI());
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(configDirectoryPath, CONFIG_FILE_GLOB_PATTERN)) {
+			for (Path filePath : stream) {
+				LOGGER.info("load: {}", filePath);
+				this.config = MAPPER.readValue(filePath.toFile(), PolicyDecisionPointConfiguration.class);
+				break;
 			}
 		}
 	}
--- sapl-pdp-embedded/src/main/java/io/sapl/prp/resources/ResourcesPolicyRetrievalPoint.java
+++ sapl-pdp-embedded/src/main/java/io/sapl/prp/resources/ResourcesPolicyRetrievalPoint.java
@@ -1,17 +1,21 @@
 package io.sapl.prp.resources;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.IOUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -36,6 +40,8 @@ public class ResourcesPolicyRetrievalPoint implements PolicyRetrievalPoint {
 
 	private static final String POLICY_FILE_GLOB_PATTERN = "*.sapl";
 
+	private static final String POLICY_FILE_SUFFIX = ".sapl";
+
 	private ParsedDocumentIndex parsedDocIdx;
 
 	public ResourcesPolicyRetrievalPoint() throws IOException, URISyntaxException, PolicyEvaluationException {
@@ -52,41 +58,69 @@ public class ResourcesPolicyRetrievalPoint implements PolicyRetrievalPoint {
 			throws IOException, URISyntaxException, PolicyEvaluationException {
 
 		URL policyFolderUrl = clazz.getResource(policyPath);
-
 		if (policyFolderUrl == null) {
 			throw new PolicyEvaluationException("Policy folder not found. Path:" + policyPath);
 		}
 
 		this.parsedDocIdx = parsedDocumentIndex;
 
-		Path path;
-		FileSystem fs = null;
+		if ("jar".equals(policyFolderUrl.getProtocol())) {
+			readPoliciesFromJar(policyFolderUrl);
+		}
+		else {
+			readPoliciesFromDirectory(policyFolderUrl);
+		}
+		this.parsedDocIdx.setLiveMode();
+	}
+
+	private void readPoliciesFromJar(URL policiesFolderUrl) throws PolicyEvaluationException {
+		LOGGER.debug("reading policies from jar {}", policiesFolderUrl);
+		final String[] jarPathElements = policiesFolderUrl.toString().split("!");
+		final String jarFilePath = jarPathElements[0].substring("jar:file:".length());
+		String policiesDirPath = "";
+		for (int i = 1; i < jarPathElements.length; i++) {
+			policiesDirPath += jarPathElements[i];
+		}
+		if (policiesDirPath.startsWith(File.separator)) {
+			policiesDirPath = policiesDirPath.substring(1);
+		}
+
+		final SAPLInterpreter interpreter = new DefaultSAPLInterpreter();
+
 		try {
-			if ("jar".equals(policyFolderUrl.getProtocol())) {
-				final Map<String, String> env = new HashMap<>();
-				final String[] array = policyFolderUrl.toString().split("!");
-				fs = FileSystems.newFileSystem(URI.create(array[0]), env);
-				path = fs.getPath(array[1]);
-			}
-			else {
-				path = Paths.get(policyFolderUrl.toURI());
-			}
-			LOGGER.info("current path: {}", path);
-			final SAPLInterpreter interpreter = new DefaultSAPLInterpreter();
-			try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, POLICY_FILE_GLOB_PATTERN)) {
-				for (Path filePath : stream) {
-					LOGGER.info("load: {}", filePath);
-					final SAPL saplDocument = interpreter.parse(Files.newInputStream(filePath));
-					this.parsedDocIdx.put(filePath.toString(), saplDocument);
+			ZipFile zipFile = new ZipFile(jarFilePath);
+			Enumeration<? extends ZipEntry> e = zipFile.entries();
+
+			while (e.hasMoreElements()) {
+				ZipEntry entry = e.nextElement();
+				if (!entry.isDirectory() && entry.getName().startsWith(policiesDirPath)
+						&& entry.getName().endsWith(POLICY_FILE_SUFFIX)) {
+					LOGGER.info("load: {}", entry.getName());
+					BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));
+					String fileContentsStr = IOUtils.toString(bis, StandardCharsets.UTF_8);
+					bis.close();
+					final SAPL saplDocument = interpreter.parse(fileContentsStr);
+					this.parsedDocIdx.put(entry.getName(), saplDocument);
 				}
 			}
 		}
-		finally {
-			if (fs != null) {
-				fs.close();
+		catch (IOException e) {
+			LOGGER.error("Error while reading config from jar", e);
+		}
+	}
+
+	private void readPoliciesFromDirectory(URL policiesFolderUrl)
+			throws IOException, URISyntaxException, PolicyEvaluationException {
+		LOGGER.debug("reading policies from directory {}", policiesFolderUrl);
+		final SAPLInterpreter interpreter = new DefaultSAPLInterpreter();
+		Path policiesDirectoryPath = Paths.get(policiesFolderUrl.toURI());
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(policiesDirectoryPath, POLICY_FILE_GLOB_PATTERN)) {
+			for (Path filePath : stream) {
+				LOGGER.info("load: {}", filePath);
+				final SAPL saplDocument = interpreter.parse(Files.newInputStream(filePath));
+				this.parsedDocIdx.put(filePath.toString(), saplDocument);
 			}
 		}
-		this.parsedDocIdx.setLiveMode();
 	}
 
 	@Override
