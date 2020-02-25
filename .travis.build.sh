ac527de0423b1f2b785259fecce6f309e6b30e88
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2020-02-25 15:33:59 +0100

    https for sonar

--- .travis.build.sh
+++ .travis.build.sh
@@ -25,7 +25,7 @@ fi
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
     if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
         echo "Building master"
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=http://sonar.ftk.de:9000  -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/* --batch-mode
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=https://sonar.ftk.de:9000  -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/* --batch-mode
         mvn deploy -DskipTests=true -Dmaven.javadoc.skip=true --batch-mode
     else
         echo "Building pull request"
