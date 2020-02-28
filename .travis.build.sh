c23969661526f7c46fc64e447585ce8300f0459a
heutelbeck <dheutelbeck@ftk.de>
2020-02-28 11:44:19 +0100

    travis script update

--- .travis.build.sh
+++ .travis.build.sh
@@ -28,8 +28,7 @@ cd ..
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
     if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
         echo "Building master"
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=https://sonar.ftk.de -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/* --batch-mode
-        mvn deploy -DskipTests=true -Dmaven.javadoc.skip=true --batch-mode
+        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install deploy sonar:sonar -Dsonar.host.url=https://sonar.ftk.de -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/* --batch-mode
     else
         echo "Building pull request"
         mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --batch-mode
