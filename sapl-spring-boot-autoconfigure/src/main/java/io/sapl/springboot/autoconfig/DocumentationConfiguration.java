1aded609dee02eba4c146844e7430638c7c02a41
heutelbeck <dheutelbeck@ftk.de>
2020-06-26 00:23:21 +0200

    automatic documentation bean fix

--- sapl-spring-boot-autoconfigure/src/main/java/io/sapl/springboot/autoconfig/DocumentationConfiguration.java
+++ sapl-spring-boot-autoconfigure/src/main/java/io/sapl/springboot/autoconfig/DocumentationConfiguration.java
@@ -1,29 +1,43 @@
 package io.sapl.springboot.autoconfig;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import io.sapl.api.pdp.PolicyDecisionPoint;
 import io.sapl.pdp.embedded.EmbeddedPolicyDecisionPoint;
 import io.sapl.spring.documentation.FunctionLibrariesDocumentation;
 import io.sapl.spring.documentation.PolicyInformationPointsDocumentation;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 @ComponentScan("io.sapl.spring")
 @AutoConfigureAfter({ PDPAutoConfiguration.class })
 public class DocumentationConfiguration {
 
 	@Bean
-	@ConditionalOnBean(EmbeddedPolicyDecisionPoint.class)
-	public PolicyInformationPointsDocumentation pipDocumentation(EmbeddedPolicyDecisionPoint pdp) {
-		return new PolicyInformationPointsDocumentation(pdp.getAttributeCtx().getDocumentation());
+	public PolicyInformationPointsDocumentation pipDocumentation(PolicyDecisionPoint pdp) {
+		LOGGER.info("Provisioning PIP Documentation Bean");
+		if (pdp instanceof EmbeddedPolicyDecisionPoint) {
+			EmbeddedPolicyDecisionPoint epdp = (EmbeddedPolicyDecisionPoint) pdp;
+			return new PolicyInformationPointsDocumentation(epdp.getAttributeCtx().getDocumentation());
+		}
+		LOGGER.info("PIP Documentation will be empty for non-embedded PDP implementations.");
+		return new PolicyInformationPointsDocumentation(new ArrayList<>());
 	}
 
 	@Bean
-	@ConditionalOnBean(EmbeddedPolicyDecisionPoint.class)
-	public FunctionLibrariesDocumentation functionDocumentation(EmbeddedPolicyDecisionPoint pdp) {
-		return new FunctionLibrariesDocumentation(pdp.getFunctionCtx().getDocumentation());
+	public FunctionLibrariesDocumentation functionDocumentation(PolicyDecisionPoint pdp) {
+		LOGGER.info("Provisioning Function Libraries Documentation Bean");
+		if (pdp instanceof EmbeddedPolicyDecisionPoint) {
+			EmbeddedPolicyDecisionPoint epdp = (EmbeddedPolicyDecisionPoint) pdp;
+			return new FunctionLibrariesDocumentation(epdp.getFunctionCtx().getDocumentation());
+		}
+		LOGGER.info("Function Libraries Documentation will be empty for non-embedded PDP implementations.");
+		return new FunctionLibrariesDocumentation(new ArrayList<>());
 	}
 }
