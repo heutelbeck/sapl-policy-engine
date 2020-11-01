aba35e364c942527ea2c6c18d9675a0d95e3d2c9
Patrick Engelberts <patrick.engelberts@gmail.com>
2020-11-01 14:49:43 +0100

    deleted deprecated view combining algorithm

--- sapl-server-ce/src/main/java/io/sapl/server/ce/views/AppNavLayout.java
+++ sapl-server-ce/src/main/java/io/sapl/server/ce/views/AppNavLayout.java
@@ -18,7 +18,6 @@ import com.vaadin.flow.router.RouterLink;
 
 import io.sapl.server.ce.views.documentation.FunctionLibrariesDocumentationView;
 import io.sapl.server.ce.views.documentation.PolicyInformationPointsDocumentationView;
-import io.sapl.server.ce.views.pdpconfiguration.CombiningAlgorithmView;
 import io.sapl.server.ce.views.pdpconfiguration.ConfigurePdp;
 import io.sapl.server.ce.views.pdpconfiguration.VariablesView;
 import io.sapl.server.ce.views.sapldocument.SaplDocumentsView;
@@ -61,7 +60,6 @@ public class AppNavLayout extends AppLayout implements AfterNavigationObserver {
 		tabs.add(createTab("Home", ShowHome.class));
 		tabs.add(createTab("SAPL Documents", SaplDocumentsView.class));
 		tabs.add(createTab("PDP Configuration", ConfigurePdp.class));
-		tabs.add(createTab("Combining Algorithm", CombiningAlgorithmView.class));
 		tabs.add(createTab("Variables", VariablesView.class));
 		tabs.add(createTab("Policy Information Points", PolicyInformationPointsDocumentationView.class));
 		tabs.add(createTab("Function Libraries", FunctionLibrariesDocumentationView.class));
