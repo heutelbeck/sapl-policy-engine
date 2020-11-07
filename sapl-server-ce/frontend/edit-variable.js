a87d553e2a636ae4afb89ea3dcb28ed3276663b0
Patrick Engelberts <patrick.engelberts@gmail.com>
2020-11-07 20:09:04 +0100

    use json editor in view to edit variable

--- sapl-server-ce/frontend/edit-variable.js
+++ sapl-server-ce/frontend/edit-variable.js
@@ -1,6 +1,7 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-text-field/src/vaadin-text-area.js';
+import '@vaadin/flow-frontend/json-editor.js';
 
 class EditVariable extends PolymerElement {
 
@@ -14,13 +15,14 @@ class EditVariable extends PolymerElement {
             </style>
 <vaadin-vertical-layout style="width: 100%; height: 100%; padding: var(--lumo-space-m);" theme="spacing">
  <vaadin-text-area label="Name" id="nameTextArea" minlength="1" has-value style="width: 300px;"></vaadin-text-area>
- <vaadin-text-area label="JSON Value" id="jsonValueTextArea" style="align-self: stretch; flex-grow: 1; width: 100%; height: 100%; flex-shrink: 1;" minlength="1"></vaadin-text-area>
+ <vaadin-text-area label="JSON Value" id="jsonValueTextArea" style="align-self: stretch; flex-grow: 1; width: 100%; flex-shrink: 1;" minlength="1"></vaadin-text-area>
+ <json-editor style="width: 100%; height: 100%;"></json-editor>
  <vaadin-horizontal-layout style="margin: var(--lumo-space-s); padding: var(--lumo-space-s); align-self: flex-end;">
   <vaadin-button theme="primary" id="editButton" style="margin: var(--lumo-space-s);">
-   Save
+    Save 
   </vaadin-button>
   <vaadin-button theme="cancel" style="margin: var(--lumo-space-s);" id="cancelButton">
-   Cancel
+    Cancel 
   </vaadin-button>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
