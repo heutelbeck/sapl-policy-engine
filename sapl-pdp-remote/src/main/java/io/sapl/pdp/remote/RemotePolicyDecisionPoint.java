80bff72af4d35a3456a4730329eaf34aefc5fbd5
heutelbeck <dheutelbeck@ftk.de>
2021-03-15 19:59:05 +0100

    Cleanup of maven build

--- sapl-pdp-remote/src/main/java/io/sapl/pdp/remote/RemotePolicyDecisionPoint.java
+++ sapl-pdp-remote/src/main/java/io/sapl/pdp/remote/RemotePolicyDecisionPoint.java
@@ -100,7 +100,7 @@ public class RemotePolicyDecisionPoint implements PolicyDecisionPoint {
 	private <T> Flux<T> decide(String path, ParameterizedTypeReference<ServerSentEvent<T>> type,
 			Object authzSubscription) {
 		return client.post().uri(path).accept(MediaType.APPLICATION_NDJSON).contentType(MediaType.APPLICATION_JSON)
-				.bodyValue(authzSubscription).retrieve().bodyToFlux(type).map(event -> event.data())
+				.bodyValue(authzSubscription).retrieve().bodyToFlux(type).map(ServerSentEvent::data)
 				.doOnError(error -> log.error("Error : {}", error.getMessage()));
 	}
 
