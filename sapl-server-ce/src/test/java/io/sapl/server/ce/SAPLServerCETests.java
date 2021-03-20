9f274c6478505c6fe3410160ddfc34aeef1393be
heutelbeck <dheutelbeck@ftk.de>
2021-03-20 01:46:12 +0100

    basic tests for servers added

---
+++ sapl-server-ce/src/test/java/io/sapl/server/ce/SAPLServerCETests.java
@@ -0,0 +1,13 @@
+package io.sapl.server.ce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SAPLServerCETests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sapl-server-lt/src/test/java/io/sapl/server/lt/SAPLServerLTTests.java
@@ -0,0 +1,13 @@
+package io.sapl.server.lt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SAPLServerLTTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
