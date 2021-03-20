c6f2c84be178f218d81b7cbf844a00e732b16006
heutelbeck <dheutelbeck@ftk.de>
2021-03-20 11:56:44 +0100

    Fix skipping of constraint handlers.

--- sapl-spring-security/src/main/java/io/sapl/spring/constraints/ConstraintHandlerService.java
+++ sapl-spring-security/src/main/java/io/sapl/spring/constraints/ConstraintHandlerService.java
@@ -21,6 +21,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import io.sapl.api.pdp.AuthorizationDecision;
 import io.sapl.api.pep.ConstraintHandler;
@@ -36,50 +37,30 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ConstraintHandlerService {
 
-	private static final String FAILED_TO_HANDLE_OBLIGATION = "Failed to handle obligation: %s";
+	static final String FAILED_TO_HANDLE_OBLIGATION = "Failed to handle obligation.";
+	static final String OBLIGATION_HANDLER_MISSING = "Obligation handler missing.";
+
+	private static final String NO_OBLIGATION_HANDLER_FOUND_FOR = "No obligation handler found for: {}";
 
 	private final List<ConstraintHandler> constraintHandlers;
 
 	/**
-	 * Checks if for all obligations in a authorization decision at least one
-	 * obligation handler is registered.
-	 * 
-	 * @param authzDecision a PDP AuthorizationDecision
-	 * @return true, if for all obligations in a authorization decision at least one
-	 *         obligation handler is registered.
-	 */
-	public boolean handlersForObligationsAvailable(AuthorizationDecision authzDecision) {
-		if (authzDecision.getObligations().isPresent()) {
-			for (JsonNode obligation : authzDecision.getObligations().get()) {
-				if (!atLeastOneHandlerForConstraintIsPresent(obligation)) {
-					log.warn("No obligation handler found for: {}", obligation);
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-
-	/**
 	 * Attempts to handle all obligations of the authorization decision and throws
-	 * AccessDeniedException on failure.
+	 * AccessDeniedException on failure. If multiple handlers for an obligation are
+	 * present, all must succeed.
 	 * 
 	 * @param authzDecision a PDP authorization decision
 	 * @throws AccessDeniedException if obligation handling fails.
 	 */
 	public void handleObligations(AuthorizationDecision authzDecision) {
-		if (!handlersForObligationsAvailable(authzDecision)) {
-			throw new AccessDeniedException("Obligation handlers missing.");
-		}
-		if (authzDecision.getObligations().isPresent()) {
-			for (JsonNode obligation : authzDecision.getObligations().get()) {
-				if (!handleConstraint(obligation)) {
-					String message = String.format(FAILED_TO_HANDLE_OBLIGATION, obligation);
-					log.warn(message);
-					throw new AccessDeniedException(message);
-				}
-			}
-		}
+		if (authzDecision.getObligations().isEmpty())
+			return;
+
+		if (!handlersForAllObligationsAreAvailable(authzDecision.getObligations().get()))
+			throw new AccessDeniedException(OBLIGATION_HANDLER_MISSING);
+
+		if (!handleConstraints(authzDecision.getObligations().get()))
+			throw new AccessDeniedException(FAILED_TO_HANDLE_OBLIGATION);
 	}
 
 	/**
@@ -89,32 +70,76 @@ public class ConstraintHandlerService {
 	 * @param authzDecision a PDP authorization decision
 	 */
 	public void handleAdvices(AuthorizationDecision authzDecision) {
-		if (authzDecision.getAdvices().isPresent()) {
-			for (JsonNode advice : authzDecision.getAdvices().get()) {
-				if (!handleConstraint(advice)) {
-					log.warn("Failed to handle advice: {}", advice);
-				}
-			}
-		}
+		if (authzDecision.getAdvices().isEmpty())
+			return;
+
+		handleConstraints(authzDecision.getObligations().get());
 	}
 
-	private boolean atLeastOneHandlerForConstraintIsPresent(JsonNode constraint) {
-		for (ConstraintHandler handler : constraintHandlers) {
-			if (handler.canHandle(constraint)) {
+	/**
+	 * Attempts to handle all constraints in the supplied array.
+	 * 
+	 * @param constraints an array of constraint JSON objects
+	 * @return true, iff all handlers that could handle
+	 */
+	private boolean handleConstraints(ArrayNode constraints) {
+		var success = true;
+		for (JsonNode constraint : constraints)
+			success &= handleConstraint(constraint);
+
+		return success;
+	}
+
+	/**
+	 * Checks if for all obligations in a authorization decision at least one
+	 * obligation handler is registered.
+	 * 
+	 * @param obligations an array of obligations
+	 * @return true, if for all obligations in a authorization decision at least one
+	 *         obligation handler is registered.
+	 */
+	private boolean handlersForAllObligationsAreAvailable(ArrayNode obligations) {
+		for (JsonNode obligation : obligations)
+			if (!atLeastOneHandlerForObligationIsPresent(obligation))
+				return false;
+
+		return true;
+	}
+
+	/**
+	 * @param constraint an obligation
+	 * @return true, iff at least one constraint handler is present for handling the
+	 *         obligation
+	 */
+	private boolean atLeastOneHandlerForObligationIsPresent(JsonNode constraint) {
+		for (ConstraintHandler handler : constraintHandlers)
+			if (handler.canHandle(constraint))
 				return true;
-			}
-		}
+
+		log.warn(NO_OBLIGATION_HANDLER_FOUND_FOR, constraint);
 		return false;
 	}
 
+	/**
+	 * This method iterates over all constraint handlers. All constraint handlers
+	 * capable of handling a constraint care called. Thus, a constraint may trigger
+	 * multiple handlers. As there is no way of prioritizing constraint handlers at
+	 * the moment this is the only meaningful option if multiple handlers for a
+	 * constraint are present.
+	 * 
+	 * A constraint is considered to be handled successfully, if all
+	 * ConstraintHandlers capable of handling the constraint succeed.
+	 * 
+	 * @param constraint a constraint
+	 * @return true, iff at least one ConstraintHandler successfully handled the
+	 *         constraint.
+	 */
 	private boolean handleConstraint(JsonNode constraint) {
 		boolean success = false;
-		for (ConstraintHandler handler : constraintHandlers) {
-			if (handler.canHandle(constraint)) {
-				// one success is sufficient for overall success, but all are attempted
-				success = success || handler.handle(constraint);
-			}
-		}
+		for (ConstraintHandler handler : constraintHandlers)
+			if (handler.canHandle(constraint))
+				success &= handler.handle(constraint);
+
 		return success;
 	}
 
