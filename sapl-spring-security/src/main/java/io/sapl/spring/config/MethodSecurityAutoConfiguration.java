f72ae19538258b062b472fd0b6602069e4bea2fc
heutelbeck <dheutelbeck@ftk.de>
2021-03-30 18:34:25 +0200

    Spring securtiy configuration tests

--- sapl-spring-security/src/main/java/io/sapl/spring/config/MethodSecurityAutoConfiguration.java
+++ sapl-spring-security/src/main/java/io/sapl/spring/config/MethodSecurityAutoConfiguration.java
@@ -15,87 +15,23 @@
  */
 package io.sapl.spring.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.AfterInvocationProvider;
-import org.springframework.security.access.intercept.AfterInvocationManager;
-import org.springframework.security.access.intercept.AfterInvocationProviderManager;
-import org.springframework.security.access.method.MethodSecurityMetadataSource;
-import org.springframework.security.access.vote.AffirmativeBased;
-import org.springframework.security.access.vote.AuthenticatedVoter;
-import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.sapl.api.pdp.PolicyDecisionPoint;
 import io.sapl.spring.constraints.ConstraintHandlerService;
-import io.sapl.spring.method.PolicyBasedEnforcementAttributeFactory;
-import io.sapl.spring.method.PolicyEnforcementMethodSecurityMetadataSource;
-import io.sapl.spring.method.post.PolicyBasedPostInvocationEnforcementAdvice;
-import io.sapl.spring.method.post.PostInvocationEnforcementProvider;
-import io.sapl.spring.method.pre.PolicyBasedPreInvocationEnforcementAdvice;
-import io.sapl.spring.method.pre.PreInvocationEnforcementAdviceVoter;
-import lombok.RequiredArgsConstructor;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableGlobalMethodSecurity
-public class MethodSecurityAutoConfiguration extends GlobalMethodSecurityConfiguration {
+public class MethodSecurityAutoConfiguration extends MethodSecurityConfiguration {
 
-	protected final ObjectFactory<PolicyDecisionPoint> pdpFactory;
-
-	protected final ObjectFactory<ConstraintHandlerService> constraintHandlerFactory;
-
-	protected final ObjectFactory<ObjectMapper> objectMapperFactory;
-
-	@Override
-	protected AccessDecisionManager accessDecisionManager() {
-		List<AccessDecisionVoter<?>> decisionVoters = new ArrayList<>();
-		PolicyBasedPreInvocationEnforcementAdvice policyAdvice = new PolicyBasedPreInvocationEnforcementAdvice(
-				pdpFactory, constraintHandlerFactory, objectMapperFactory);
-		policyAdvice.setExpressionHandler(getExpressionHandler());
-		decisionVoters.add(new PreInvocationEnforcementAdviceVoter(policyAdvice));
-		decisionVoters.add(new RoleVoter());
-		decisionVoters.add(new AuthenticatedVoter());
-		AffirmativeBased manager = new AffirmativeBased(decisionVoters);
-		manager.setAllowIfAllAbstainDecisions(true);
-		return manager;
-	}
-
-	@Override
-	protected AfterInvocationManager afterInvocationManager() {
-		PolicyBasedPostInvocationEnforcementAdvice advice = new PolicyBasedPostInvocationEnforcementAdvice(pdpFactory,
-				constraintHandlerFactory, objectMapperFactory);
-		advice.setExpressionHandler(getExpressionHandler());
-		PostInvocationEnforcementProvider provider = new PostInvocationEnforcementProvider(advice);
-
-		AfterInvocationProviderManager invocationProviderManager = (AfterInvocationProviderManager) super.afterInvocationManager();
-		if (invocationProviderManager == null) {
-			invocationProviderManager = new AfterInvocationProviderManager();
-			List<AfterInvocationProvider> afterInvocationProviders = new ArrayList<>();
-			afterInvocationProviders.add(provider);
-			invocationProviderManager.setProviders(afterInvocationProviders);
-		} else {
-			List<AfterInvocationProvider> originalProviders = invocationProviderManager.getProviders();
-			List<AfterInvocationProvider> afterInvocationProviders = new ArrayList<>();
-			afterInvocationProviders.add(provider);
-			afterInvocationProviders.addAll(originalProviders);
-			invocationProviderManager.setProviders(afterInvocationProviders);
-		}
-		return invocationProviderManager;
-	}
-
-	@Override
-	protected MethodSecurityMetadataSource customMethodSecurityMetadataSource() {
-		return new PolicyEnforcementMethodSecurityMetadataSource(
-				new PolicyBasedEnforcementAttributeFactory(getExpressionHandler()));
+	public MethodSecurityAutoConfiguration(ObjectFactory<PolicyDecisionPoint> pdpFactory,
+			ObjectFactory<ConstraintHandlerService> constraintHandlerFactory,
+			ObjectFactory<ObjectMapper> objectMapperFactory) {
+		super(pdpFactory, constraintHandlerFactory, objectMapperFactory);
 	}
 
 }
