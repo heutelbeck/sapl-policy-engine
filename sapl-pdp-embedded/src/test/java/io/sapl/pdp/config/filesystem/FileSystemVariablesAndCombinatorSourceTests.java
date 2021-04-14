6f18284012bacee0e0cb22eaeaa235c7891e5d49
heutelbeck <dheutelbeck@ftk.de>
2021-04-14 09:41:59 +0200

    fixed test

--- sapl-pdp-embedded/src/test/java/io/sapl/pdp/config/filesystem/FileSystemVariablesAndCombinatorSourceTests.java
+++ sapl-pdp-embedded/src/test/java/io/sapl/pdp/config/filesystem/FileSystemVariablesAndCombinatorSourceTests.java
@@ -25,68 +25,53 @@ import io.sapl.util.filemonitoring.FileDeletedEvent;
 import io.sapl.util.filemonitoring.FileMonitorUtil;
 import reactor.core.publisher.Flux;
 
-
 class FileSystemVariablesAndCombinatorSourceTest {
 
-    @Test
-    void loadExistingConfigTest() {
-        var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources/policies");
-        var algo = configProvider.getCombiningAlgorithm().blockFirst();
-        var variables = configProvider.getVariables().blockFirst();
-        configProvider.dispose();
+	@Test
+	void loadExistingConfigTest() {
+		var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources/policies");
+		var algo = configProvider.getCombiningAlgorithm().blockFirst();
+		var variables = configProvider.getVariables().blockFirst();
+		configProvider.dispose();
 
-        assertThat(algo.get() instanceof DenyUnlessPermitCombiningAlgorithm, is(true));
-        assertThat(variables.get().size(), is(3));
-    }
-
-
-    @Test
-    void return_default_config_for_missing_configuration_file() {
-        var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources");
-        var algo = configProvider.getCombiningAlgorithm().blockFirst();
-        var variables = configProvider.getVariables().blockFirst();
-        configProvider.dispose();
-
-        assertThat(algo.get() instanceof DenyUnlessPermitCombiningAlgorithm, is(true));
-        assertThat(variables.get().size(), is(0));
-    }
+		assertThat(algo.get() instanceof DenyUnlessPermitCombiningAlgorithm, is(true));
+		assertThat(variables.get().size(), is(3));
+	}
 
 	@Test
-    @SuppressWarnings("unchecked")
-    void return_empty_optional_for_exception_during_config_load() throws Exception {
-        try (MockedConstruction<ObjectMapper> mocked = Mockito.mockConstruction(ObjectMapper.class,
-                (mock, context) -> {
-                    doThrow(new IOException()).when(mock).readValue(ArgumentMatchers.any(File.class), ArgumentMatchers.any(Class.class));
-                })) {
+	void return_default_config_for_missing_configuration_file() {
+		var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources");
+		var algo = configProvider.getCombiningAlgorithm().blockFirst();
+		var variables = configProvider.getVariables().blockFirst();
+		configProvider.dispose();
 
-            var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources/policies");
-            var algo = configProvider.getCombiningAlgorithm().blockFirst();
-            var variables = configProvider.getVariables().blockFirst();
-            configProvider.dispose();
+		assertThat(algo.get() instanceof DenyUnlessPermitCombiningAlgorithm, is(true));
+		assertThat(variables.get().size(), is(0));
+	}
 
-            verify(mocked.constructed().get(0), times(1)).
-                    readValue(ArgumentMatchers.any(File.class), ArgumentMatchers.any(Class.class));
+	@Test
+	void return_empty_optional_for_exception_during_config_load() throws Exception {
+		var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources/broken_config");
+		var algo = configProvider.getCombiningAlgorithm().blockFirst();
+		var variables = configProvider.getVariables().blockFirst();
+		configProvider.dispose();
+		assertThat(algo.isEmpty(), is(true));
+		assertThat(variables.isEmpty(), is(true));
+	}
 
-            assertThat(algo.isEmpty(), is(true));
-            assertThat(variables.isEmpty(), is(true));
-        }
-    }
+	@Test
+	void test_process_watcher_event() {
+		try (MockedStatic<FileMonitorUtil> mock = mockStatic(FileMonitorUtil.class)) {
+			mock.when(() -> FileMonitorUtil.monitorDirectory(any(), any()))
+					.thenReturn(Flux.just(new FileCreatedEvent(null), new FileDeletedEvent(null)));
 
-    @Test
-    void test_process_watcher_event() {
-        try (MockedStatic<FileMonitorUtil> mock = mockStatic(FileMonitorUtil.class)) {
-            mock.when(() -> FileMonitorUtil.monitorDirectory(any(), any()))
-                    .thenReturn(Flux.just(new FileCreatedEvent(null), new FileDeletedEvent(null)));
+			var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources/policies");
+			var algo = configProvider.getCombiningAlgorithm().blockLast();
+			configProvider.getVariables().blockFirst();
+			configProvider.dispose();
 
-
-            var configProvider = new FileSystemVariablesAndCombinatorSource("src/test/resources/policies");
-            var algo = configProvider.getCombiningAlgorithm().blockLast();
-            configProvider.getVariables().blockFirst();
-            configProvider.dispose();
-
-
-            mock.verify(() -> FileMonitorUtil.monitorDirectory(any(), any()), times(1));
-            assertThat(algo.get() instanceof DenyUnlessPermitCombiningAlgorithm, is(true));
-        }
-    }
+			mock.verify(() -> FileMonitorUtil.monitorDirectory(any(), any()), times(1));
+			assertThat(algo.get() instanceof DenyUnlessPermitCombiningAlgorithm, is(true));
+		}
+	}
 }
