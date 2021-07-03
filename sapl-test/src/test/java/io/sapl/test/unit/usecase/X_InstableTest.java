516447290482ae3925f97c05165ebc15cb255f57
Nikolai Seip <nikolai.seip@gmail.com>
2021-07-03 10:09:25 +0200

    [sapl-test] enable instable test with TRACE logging

---
+++ sapl-test/src/test/java/io/sapl/test/unit/usecase/X_InstableTest.java
@@ -0,0 +1,48 @@
+package io.sapl.test.unit.usecase;
+
+import static io.sapl.hamcrest.Matchers.anyAuthDecision;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.sapl.api.pdp.AuthorizationSubscription;
+import io.sapl.functions.TemporalFunctionLibrary;
+import io.sapl.interpreter.InitializationException;
+import io.sapl.pip.ClockPolicyInformationPoint;
+import io.sapl.test.SaplTestFixture;
+import io.sapl.test.unit.SaplUnitTestFixture;
+
+class X_InstableTest {
+	
+	private SaplTestFixture fixture;
+	
+	@BeforeEach
+	void setUp() throws InitializationException {
+		fixture = new SaplUnitTestFixture("policyStreaming")
+				//.registerPIP(null)
+				.registerFunctionLibrary(new TemporalFunctionLibrary());
+	}
+
+	@Test
+	void test_streamingPolicyWithVirtualTime() throws InitializationException {
+		
+		fixture.registerPIP(new ClockPolicyInformationPoint())
+			.constructTestCaseWithMocks()
+			.withVirtualTime()
+			.when(AuthorizationSubscription.of("ROLE_DOCTOR", "read", "heartBeatData"))
+			.expectNext(anyAuthDecision())
+			.thenAwait(Duration.ofSeconds(2))
+			.expectNext(anyAuthDecision())
+			.thenAwait(Duration.ofSeconds(2))
+			.expectNext(anyAuthDecision())
+			.thenAwait(Duration.ofSeconds(2))
+			.expectNext(anyAuthDecision())
+			.thenAwait(Duration.ofSeconds(2))
+			.expectNext(anyAuthDecision())
+			.thenAwait(Duration.ofSeconds(2))
+			.expectNext(anyAuthDecision())
+			.verify();
+	}
+}
