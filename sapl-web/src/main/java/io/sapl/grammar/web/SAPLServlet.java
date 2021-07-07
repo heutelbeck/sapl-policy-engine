176771a6288f722a8a40aa6a39cfc0449b6d9575
Dominic Heutelbeck <dominic.heutelbeck@gmail.com>
2021-07-07 21:55:11 +0200

    sonarqube issues fix

--- sapl-web/src/main/java/io/sapl/grammar/web/SAPLServlet.java
+++ sapl-web/src/main/java/io/sapl/grammar/web/SAPLServlet.java
@@ -19,26 +19,27 @@ import javax.servlet.annotation.WebServlet;
 
 import org.eclipse.xtext.util.DisposableRegistry;
 import org.eclipse.xtext.web.servlet.XtextServlet;
-import org.eclipse.xtext.xbase.lib.Exceptions;
 
 import com.google.inject.Injector;
+
+import lombok.SneakyThrows;
 
 /**
  * Deploy this class into a servlet container to enable DSL-specific services.
  */
 @WebServlet(name = "XtextServices", urlPatterns = "/xtext-service/*")
 public class SAPLServlet extends XtextServlet {
+
+	private static final long serialVersionUID = 1L;
+
 	private transient DisposableRegistry disposableRegistry;
 
 	@Override
+	@SneakyThrows
 	public void init() {
-		try {
-			super.init();
-			final Injector injector = new SAPLWebSetup().createInjectorAndDoEMFRegistration();
-			disposableRegistry = injector.<DisposableRegistry>getInstance(DisposableRegistry.class);
-		} catch (Throwable throwable) {
-			throw Exceptions.sneakyThrow(throwable);
-		}
+		super.init();
+		final Injector injector = new SAPLWebSetup().createInjectorAndDoEMFRegistration();
+		disposableRegistry = injector.<DisposableRegistry>getInstance(DisposableRegistry.class);
 	}
 
 	@Override
