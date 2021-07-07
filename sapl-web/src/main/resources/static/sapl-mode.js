fd2837b3105ce52f19c3f3a9b357460614215051
Dominic Heutelbeck <dominic.heutelbeck@gmail.com>
2021-07-07 09:43:25 +0200

    cleanup build logging

--- sapl-web/src/main/resources/static/sapl-mode.js
+++ sapl-web/src/main/resources/static/sapl-mode.js
@@ -1,5 +1,5 @@
 define("sapl-mode", ["codemirror", "codemirror/addon/mode/simple"], function(CodeMirror, SimpleMode) {
-	var keywords = "advice|as|deny|each|false|for|import|in|null|obligation|permit|policy|set|transform|true|undefined|var|where";
+	var keywords = "where|var|undefined|true|transform|set|policy|permit-unless-deny|permit-overrides|permit|only-one-applicable|obligation|null|in|import|for|first-applicable|false|each|deny-unless-permit|deny-overrides|deny|as|advice";
 	CodeMirror.defineSimpleMode("xtext/sapl", {
 		start: [
 			{token: "comment", regex: "\\/\\/.*$"},
