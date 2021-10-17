6da5919938cdb75a284129839ca2e670b481c5d1
Nikolai Seip <nikolai.seip@gmail.com>
2021-10-17 18:29:37 +0200

    [sapl-test] rewrite AttributeMockTestPublisher
    use Sinks.many().replay() instead of TestPublisher with two effects:
    1. no NullPointerException in FluxSwitchMap when second policy in set is subscribing to the same TestPublisher (this NPE happens for a Sinks.many().multicast().onBackpressureBuffer() too, but not for a replay sink)
    2. fix a bug that the second policy in a set using the same attribute is getting a different Val from the attribute

--- sapl-test/src/main/java/io/sapl/test/mocking/AttributeMockTestPublisher.java
+++ sapl-test/src/main/java/io/sapl/test/mocking/AttributeMockTestPublisher.java
@@ -1,58 +1,48 @@
 package io.sapl.test.mocking;
 
 import static io.sapl.test.Imports.times;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import io.sapl.api.interpreter.Val;
-import io.sapl.test.SaplTestException;
 import io.sapl.test.verification.MockRunInformation;
 import io.sapl.test.verification.MockingVerification;
+
 import reactor.core.publisher.Flux;
-import reactor.test.publisher.TestPublisher;
+import reactor.core.publisher.Sinks;
 
 public class AttributeMockTestPublisher implements AttributeMock {
 	private static final String ERROR_DUPLICATE_MOCK_REGISTRATION_TIMING_MODE = "You already defined a Mock for %s which is returning specified values";
 	private final String fullname;
 	
 	
-	private TestPublisher<Val> testpublisher;
+	private Sinks.Many<Val> publisher;
+	private Flux<Val> returnFlux;
 
 	private final MockRunInformation mockRunInformation;
 	private final List<MockingVerification> listMockingVerifications;
 
 	public AttributeMockTestPublisher(String fullname) {
-		this.testpublisher = null;
 		this.fullname = fullname;
-
 		this.mockRunInformation = new MockRunInformation(fullname);
 		this.listMockingVerifications = new LinkedList<>();
-	}
+		this.listMockingVerifications.add(times(greaterThanOrEqualTo(1)));
+		
+		this.publisher = Sinks.many().replay().latest();
+		this.returnFlux = this.publisher.asFlux();
 
-	public void markMock() {
-		this.testpublisher = TestPublisher.<Val>create();
-
-		this.listMockingVerifications.add(times(1));
 	}
 
 	public void mockEmit(Val returns) {
-		if (this.testpublisher == null) {
-			throw new SaplTestException("Undefined internal state. Please report a bug to the library authors!");
-		}
-		this.testpublisher.next(returns);
+		this.publisher.tryEmitNext(returns);
 	}
 
 	@Override
 	public Flux<Val> evaluate() {
-		// TODO check if call parameter for attributes should stay irrelevant
 		this.mockRunInformation.saveCall(new FunctionCallSimple());
-
-		if (this.testpublisher == null) {
-			throw new SaplTestException("Undefined internal state. Please report a bug to the library authors!");
-		}
-
-		return this.testpublisher.flux();
+		return this.returnFlux;
 	}
 
 	@Override
--- sapl-test/src/test/java/io/sapl/test/mocking/AttributeMockTestPublisherTest.java
+++ sapl-test/src/test/java/io/sapl/test/mocking/AttributeMockTestPublisherTest.java
@@ -1,10 +1,10 @@
 package io.sapl.test.mocking;
 
+import io.sapl.api.interpreter.Val;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import io.sapl.api.interpreter.Val;
-import io.sapl.test.SaplTestException;
 import reactor.test.StepVerifier;
 
 public class AttributeMockTestPublisherTest {
@@ -12,8 +12,6 @@ public class AttributeMockTestPublisherTest {
 	@Test
 	void test() {
 		AttributeMockTestPublisher mock = new AttributeMockTestPublisher("foo.bar");
-		
-		mock.markMock();
 
 		StepVerifier.create(mock.evaluate())
 			.then(() -> mock.mockEmit(Val.of(1)))
@@ -21,18 +19,6 @@ public class AttributeMockTestPublisherTest {
 			.thenCancel().verify();
 		
 		mock.assertVerifications();
-	}
-	
-	@Test
-	void test_unmocked() {
-		AttributeMockTestPublisher mock = new AttributeMockTestPublisher("foo.bar");
-		
-		Assertions.assertThatExceptionOfType(SaplTestException.class)
-		.isThrownBy(() -> mock.evaluate());
-		
-		Assertions.assertThatExceptionOfType(SaplTestException.class)
-		.isThrownBy(() -> mock.mockEmit(Val.of(1)));
-		
 	}
 	
 	@Test
