40d73e198eefd1ebce2be2e0eab5a32363baa51a
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2022-07-31 16:55:40 +0200

    fix test type confusion

--- sapl-spring-security/src/main/java/io/sapl/spring/config/ObjectMapperAutoConfiguration.java
+++ sapl-spring-security/src/main/java/io/sapl/spring/config/ObjectMapperAutoConfiguration.java
@@ -48,7 +48,7 @@ public class ObjectMapperAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
-	public ObjectMapper objectMapper() {
+	ObjectMapper objectMapper() {
 		return new ObjectMapper();
 	}
 
--- sapl-spring-security/src/test/java/io/sapl/spring/subscriptions/AuthorizationSubscriptionBuilderServiceTests.java
+++ sapl-spring-security/src/test/java/io/sapl/spring/subscriptions/AuthorizationSubscriptionBuilderServiceTests.java
@@ -24,6 +24,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -57,6 +58,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.server.ServerWebExchange;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -150,11 +152,11 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 										.where("name", jsonText("publicVoid")))))),
 				() -> assertThat(subscription.getResource(),
 						is(jsonObject()
-								.where("java", is(jsonObject()
-										.where("instanceof", 
-											is(jsonArray(org.hamcrest.Matchers.hasItems(
-												jsonObject()
-													.where("simpleName", is(jsonText("TestClass"))))))))))),											
+								  .where("java", is(jsonObject()
+										.where("instanceof",												
+											is(jsonArray(containsInAnyOrder(
+												jsonObject().where("simpleName",is(jsonText("TestClass"))),
+												jsonObject().where("simpleName",is(jsonText("Object"))))))))))),
 				() -> assertThat(subscription.getEnvironment(), is(jsonNull())));
 		// @formatter:on
 	}
@@ -182,11 +184,12 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 	}
 
 	@Test
-	void when_nullParametersAndAnonymousAuthentication_then_FactoryConstructsFromContextAndNoAuthn() {
+	void when_nullParametersAndAnonymousAuthentication_then_FactoryConstructsFromContextAndNoAuthn() throws JsonProcessingException {
 		var attribute = attribute(null, null, null, null, Object.class);
 		var anonymous = new AnonymousAuthenticationToken("key", "anonymous",
 				AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
 		var subscription = defaultBuilderUnderTest.constructAuthorizationSubscription(anonymous, invocation, attribute);
+		
 		// @formatter:off
 		assertAll(() -> assertThat(subscription.getSubject(),
 						  is(jsonObject()
@@ -201,11 +204,11 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 				  () -> assertThat(subscription.getResource(),
 						  is(jsonObject()
 								  .where("http", is(jsonMissing()))
-									.where("java", is(jsonObject()
-											.where("instanceof", 
-												is(jsonArray(org.hamcrest.Matchers.hasItems(
-													jsonObject()
-														.where("simpleName", is(jsonText("TestClass"))))))))))),											
+								  .where("java", is(jsonObject()
+										.where("instanceof",												
+											is(jsonArray(containsInAnyOrder(
+												jsonObject().where("simpleName",is(jsonText("TestClass"))),
+												jsonObject().where("simpleName",is(jsonText("Object"))))))))))),
 			      () -> assertThat(subscription.getEnvironment(), is(jsonNull())));
 		// @formatter:on
 	}
@@ -235,11 +238,11 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 					() -> assertThat(subscription.getResource(),
 							is(jsonObject()
 									.where("http", is(jsonObject()))
-									.where("java", is(jsonObject()
-										.where("instanceof", 
-											is(jsonArray(org.hamcrest.Matchers.hasItems(
-												jsonObject()
-													.where("simpleName", is(jsonText("TestClass"))))))))))),											
+									  .where("java", is(jsonObject()
+												.where("instanceof",												
+													is(jsonArray(containsInAnyOrder(
+														jsonObject().where("simpleName",is(jsonText("TestClass"))),
+														jsonObject().where("simpleName",is(jsonText("Object"))))))))))),
 					() -> assertThat(subscription.getEnvironment(), is(jsonNull())));
 			// @formatter:on
 		}
@@ -265,11 +268,11 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 										.where("name", jsonText("publicVoidArgs")))))),
 				() -> assertThat(subscription.getResource(),
 						is(jsonObject()
-								.where("java", is(jsonObject()
-										.where("instanceof", 
-											is(jsonArray(org.hamcrest.Matchers.hasItems(
-												jsonObject()
-													.where("simpleName", is(jsonText("TestClass"))))))))))),											
+								  .where("java", is(jsonObject()
+										.where("instanceof",												
+											is(jsonArray(containsInAnyOrder(
+												jsonObject().where("simpleName",is(jsonText("TestClass"))),
+												jsonObject().where("simpleName",is(jsonText("Object"))))))))))),
 				() -> assertThat(subscription.getEnvironment(), is(jsonNull())));
 		// @formatter:on
 
@@ -296,11 +299,11 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 										.where("name", jsonText("publicVoidProblemArg")))))),
 				() -> assertThat(subscription.getResource(),
 						is(jsonObject()
-								.where("java", is(jsonObject()
-										.where("instanceof", 
-											is(jsonArray(org.hamcrest.Matchers.hasItems(
-												jsonObject()
-													.where("simpleName", is(jsonText("TestClass"))))))))))),											
+								  .where("java", is(jsonObject()
+										.where("instanceof",												
+											is(jsonArray(containsInAnyOrder(
+												jsonObject().where("simpleName",is(jsonText("TestClass"))),
+												jsonObject().where("simpleName",is(jsonText("Object"))))))))))),
 				() -> assertThat(subscription.getEnvironment(), is(jsonNull())));
 		// @formatter:on
 	}
@@ -326,11 +329,11 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 										.where("name", jsonText("publicVoid")))))),
 				() -> assertThat(subscription.getResource(),
 						is(jsonObject()
-								.where("java", is(jsonObject()
-										.where("instanceof", 
-											is(jsonArray(org.hamcrest.Matchers.hasItems(
-												jsonObject()
-													.where("simpleName", is(jsonText("TestClass"))))))))))),											
+								  .where("java", is(jsonObject()
+										.where("instanceof",												
+											is(jsonArray(containsInAnyOrder(
+												jsonObject().where("simpleName",is(jsonText("TestClass"))),
+												jsonObject().where("simpleName",is(jsonText("Object"))))))))))),
 				() -> assertThat(subscription.getEnvironment(), is(jsonNull())));
 		// @formatter:on
 	}
@@ -354,11 +357,11 @@ public class AuthorizationSubscriptionBuilderServiceTests {
 				  () -> assertThat(subscription.getResource(),
 						  is(jsonObject()
 								  .where("http", is(jsonMissing()))
-									.where("java", is(jsonObject()
-											.where("instanceof", 
-												is(jsonArray(org.hamcrest.Matchers.hasItems(
-													jsonObject()
-														.where("simpleName", is(jsonText("TestClass"))))))))))),											
+								  .where("java", is(jsonObject()
+										.where("instanceof",												
+											is(jsonArray(containsInAnyOrder(
+												jsonObject().where("simpleName",is(jsonText("TestClass"))),
+												jsonObject().where("simpleName",is(jsonText("Object"))))))))))),
 			      () -> assertThat(subscription.getEnvironment(), is(jsonNull())));
 		// @formatter:on
 	}
