53d451e8915a9a1c13cf4b764ccecaf3801685e7
Martin Schweyen <mschweyen@gmail.com>
2023-05-12 23:34:19 +0200

    fix visibility of RemoteRsocketPolicyDecisionPoint.builder

--- sapl-pdp-remote/src/main/java/io/sapl/pdp/remote/RemoteRsocketPolicyDecisionPoint.java
+++ sapl-pdp-remote/src/main/java/io/sapl/pdp/remote/RemoteRsocketPolicyDecisionPoint.java
@@ -123,7 +123,7 @@ public class RemoteRsocketPolicyDecisionPoint implements PolicyDecisionPoint {
 				.doOnError(error -> log.error("RSocket Connect Error : error {}", error.getMessage(), error));
 	}
 
-	static RemoteRsocketPolicyDecisionPointBuilder builder() {
+	public static RemoteRsocketPolicyDecisionPointBuilder builder() {
 		return new RemoteRsocketPolicyDecisionPointBuilder();
 	}
 
