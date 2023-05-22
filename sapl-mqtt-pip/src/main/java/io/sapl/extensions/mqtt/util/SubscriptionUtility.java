04966e4754d61f88f7cfce8bb755bb8a74725b57
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2023-05-23 01:07:27 +0200

    update license headers

--- sapl-mqtt-pip/src/main/java/io/sapl/extensions/mqtt/util/SubscriptionUtility.java
+++ sapl-mqtt-pip/src/main/java/io/sapl/extensions/mqtt/util/SubscriptionUtility.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019-2022 Dominic Heutelbeck (dominic@heutelbeck.com)
+ * Copyright © 2017-2023 Dominic Heutelbeck (dominic@heutelbeck.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.sapl.extensions.mqtt.util;
 
 import static io.sapl.extensions.mqtt.util.ConfigUtility.getQos;
