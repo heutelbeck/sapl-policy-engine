f2475c6cdbe201ea58e2f4a1af437bef0997f8cf
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2023-10-27 00:43:43 +0200

    fix up headers again

--- sapl-test/src/test/java/io/sapl/test/utils/ClasspathHelperTests.java
+++ sapl-test/src/test/java/io/sapl/test/utils/ClasspathHelperTests.java
@@ -1,5 +1,4 @@
 /*
- * Streaming Attribute Policy Language (SAPL) Engine
  * Copyright (C) 2017-2023 Dominic Heutelbeck (dominic@heutelbeck.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
