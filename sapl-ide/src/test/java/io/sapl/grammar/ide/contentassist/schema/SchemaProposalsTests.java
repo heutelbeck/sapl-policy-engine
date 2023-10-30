dc02a8741c1fe9878212c2a21016c5b1a8fd2d16
Anja Mirenska <a.mirenska@gmail.com>
2023-10-30 16:43:38 +0100

    Update SchemaProposalsTests.java

--- sapl-ide/src/test/java/io/sapl/grammar/ide/contentassist/schema/SchemaProposalsTests.java
+++ sapl-ide/src/test/java/io/sapl/grammar/ide/contentassist/schema/SchemaProposalsTests.java
@@ -1,27 +1,19 @@
 package io.sapl.grammar.ide.contentassist.schema;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.sapl.pdp.config.VariablesAndCombinatorSource;
-import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.Test;
-import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -34,5 +26,21 @@ class SchemaProposalsTests {
         var proposals = new SchemaProposals(source);
         var variables = proposals.getVariableNamesAsTemplates();
         assertThat(variables, is(empty()));
+    }
+
+    @Test
+    void variableNamesAreReturnedWhenExistent (){
+        var mapper = new ObjectMapper();
+        var nullNode = mapper.nullNode();
+        var vars = new HashMap<String, JsonNode>();
+        vars.put("variableName", nullNode);
+
+        var source = mock(VariablesAndCombinatorSource.class);
+        when(source.getVariables()).thenReturn(Flux.just(Optional.ofNullable(vars)));
+
+        var proposals = new SchemaProposals(source);
+        var actual = proposals.getVariableNamesAsTemplates();
+        var expected = List.of("variableName");
+        assertThat(actual, equalTo(expected));
     }
 }
