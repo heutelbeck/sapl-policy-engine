ca9470cf2ffb0e294eee715991094ae5305f0766
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-01-11 15:29:22 +0100

    fix object error propagation

---
+++ sapl-lang/src/test/java/io/sapl/grammar/sapl/impl/ObjectImplCustomTests.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017-2024 Dominic Heutelbeck (dominic@heutelbeck.com)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sapl.grammar.sapl.impl;
+
+import static io.sapl.testutil.TestUtil.assertExpressionEvaluatesTo;
+import static io.sapl.testutil.TestUtil.assertExpressionReturnsErrors;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ObjectImplCustomTests {
+
+    @Test
+    void objectPropagatesErrors() {
+        assertExpressionReturnsErrors("{ \"a\": 1/0 }");
+    }
+
+    private static Stream<Arguments> provideStringsForexpressionEvaluatesToExpectedValue() {
+        // @formatter:off
+		return Stream.of(
+	 			// simpleObjectToVal
+	 			Arguments.of("{ \"a\": true, \"b\": false }","{ \"a\": true, \"b\": false }"),
+
+	 			// emptyObject
+	 			Arguments.of("{}", "{}"),
+
+	 			// dropsUndefined
+                Arguments.of("{ \"a\": true, \"b\": false, \"c\": undefined }","{ \"a\": true, \"b\": false }")
+	 		);
+		// @formater:on
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideStringsForexpressionEvaluatesToExpectedValue")
+	void expressionEvaluatesToExpectedValue(String expression, String expected) {
+		assertExpressionEvaluatesTo(expression, expected);
+	}
+}
