328180899071d4b96516f527f094f431e1ec6e3b
marcbaitinger <148320142+marcbaitinger@users.noreply.github.com>
2024-01-31 17:17:23 +0100

    jquery is no longer needed with current codemirror5 version (#120)

--- sapl-vaadin-editor/src/main/java/io/sapl/vaadin/SaplTestEditor.java
+++ sapl-vaadin-editor/src/main/java/io/sapl/vaadin/SaplTestEditor.java
@@ -36,7 +36,6 @@ import elemental.json.JsonObject;
  */
 @Tag("sapl-test-editor")
 @JsModule("./sapl-test-editor.js")
-@NpmPackage(value = "jquery", version = "3.7.1")
 @NpmPackage(value = "codemirror", version = "5.65.16")
 public class SaplTestEditor extends BaseEditor {
 
