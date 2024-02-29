3df9574ef0b2a8ee2c4c11e2b89f985467b72187
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-02-29 13:21:15 +0100

    formatting

--- sapl-test-web/src/main/java/io/sapl/test/grammar/web/SAPLTestWebSetup.java
+++ sapl-test-web/src/main/java/io/sapl/test/grammar/web/SAPLTestWebSetup.java
@@ -14,10 +14,11 @@ import org.eclipse.xtext.util.Modules2;
  * Initialization support for running Xtext languages in web applications.
  */
 public class SAPLTestWebSetup extends SAPLTestStandaloneSetup {
-	
-	@Override
-	public Injector createInjector() {
-		return Guice.createInjector(Modules2.mixin(new SAPLTestRuntimeModule(), new SAPLTestIdeModule(), new SAPLTestWebModule()));
-	}
-	
+
+    @Override
+    public Injector createInjector() {
+        return Guice.createInjector(
+                Modules2.mixin(new SAPLTestRuntimeModule(), new SAPLTestIdeModule(), new SAPLTestWebModule()));
+    }
+
 }
