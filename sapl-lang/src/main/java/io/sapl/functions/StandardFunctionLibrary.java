dd627aba184d4fce66f53121042aa7eb3ed130cf
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-05-01 19:43:46 +0200

    add set operations to standard library

--- sapl-lang/src/main/java/io/sapl/functions/StandardFunctionLibrary.java
+++ sapl-lang/src/main/java/io/sapl/functions/StandardFunctionLibrary.java
@@ -17,7 +17,10 @@
  */
 package io.sapl.functions;
 
+import java.util.function.BiFunction;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import io.sapl.api.functions.Function;
@@ -50,9 +53,131 @@ public class StandardFunctionLibrary {
             + "For NULL it returns a JSON node representing the empty string. "
             + "For ARRAY or OBJECT the function will return an error.";
 
+    private static final String CONCATENATE_DOC = "concatenate(ARRAY...arrays): Creates a new array concatenating the parameter arrays.";
+
+    private static final String INTERSECT_DOC = """
+            intersect(ARRAY...arrays): Creates a new array only containing elements present in all parameter arrays, but removing all duplicate elements. \
+            Attention: numerically equivalent but differently written, i.e., 0 vs 0.000, numbers may be interpreted as non-eqivalent.
+            """;
+
+    private static final String UNION_DOC = """
+            toSet(ARRAY..arrays): Creates a copy of the arrays containing all elements of the provided arrays, but removing all duplicate elements. \
+            Attention: numerically equivalent but differently written, i.e., 0 vs 0.000, numbers may be interpreted as non-eqivalent.
+            """;
+
+    private static final String TO_SET_DOC = """
+            toSet(ARRAY): Creates a copy of the array preserving the original order, but removing all duplicate elements. \
+            Attention: numerically equivalent but differently written, i.e., 0 vs 0.000, numbers may be interpreted as non-eqivalent.
+            """;
+
+    private static final String DIFFERENCE_DOC = """
+            difference(ARRAY,ARRAY): Returns the difference between the first and the second array, removing duplicates. \
+            Attention: numerically equivalent but differently written, i.e., 0 vs 0.000, numbers may be interpreted as non-eqivalent.
+            """;
+
     public static final String XML_TO_JSON_DOC = "xmlToJson(TEXT) Converts XML to JSON";
 
     private static final XmlMapper XML_MAPPER = new XmlMapper();
+
+    @Function(docs = CONCATENATE_DOC)
+    public static Val concatenate(@Array Val... arrays) {
+        var newArray = Val.JSON.arrayNode();
+        for (var array : arrays) {
+            var jsonArray        = array.getArrayNode();
+            var elementsIterator = jsonArray.elements();
+            while (elementsIterator.hasNext()) {
+                newArray.add(elementsIterator.next().deepCopy());
+            }
+        }
+        return Val.of(newArray);
+    }
+
+    @Function(docs = DIFFERENCE_DOC)
+    public static Val difference(@Array Val array1, @Array Val array2) {
+        var newArray         = Val.JSON.arrayNode();
+        var jsonArray        = array1.getArrayNode();
+        var elementsIterator = jsonArray.elements();
+        while (elementsIterator.hasNext()) {
+            var nextElement = elementsIterator.next();
+            if (!contains(nextElement, array2.getArrayNode(), (a, b) -> a.equals(b))
+                    && !contains(nextElement, newArray, (a, b) -> a.equals(b))) {
+                newArray.add(nextElement.deepCopy());
+            }
+        }
+        return Val.of(newArray);
+    }
+
+    @Function(docs = UNION_DOC)
+    public static Val union(@Array Val... arrays) {
+        var newArray = Val.JSON.arrayNode();
+        for (var array : arrays) {
+            var jsonArray        = array.getArrayNode();
+            var elementsIterator = jsonArray.elements();
+            while (elementsIterator.hasNext()) {
+                var nextElement = elementsIterator.next();
+                if (!contains(nextElement, newArray, (a, b) -> a.equals(b))) {
+                    newArray.add(nextElement.deepCopy());
+                }
+            }
+        }
+        return Val.of(newArray);
+    }
+
+    @Function(docs = TO_SET_DOC)
+    public static Val toSet(@Array Val array) {
+        var newArray         = Val.JSON.arrayNode();
+        var jsonArray        = array.getArrayNode();
+        var elementsIterator = jsonArray.elements();
+        while (elementsIterator.hasNext()) {
+            var nextElement = elementsIterator.next();
+            if (!contains(nextElement, newArray, (a, b) -> a.equals(b))) {
+                newArray.add(nextElement.deepCopy());
+            }
+        }
+        return Val.of(newArray);
+    }
+
+    @Function(docs = INTERSECT_DOC)
+    public static Val intersect(@Array Val... arrays) {
+        return intersect(arrays, (a, b) -> a.equals(b));
+    }
+
+    private static Val intersect(Val[] arrays, BiFunction<JsonNode, JsonNode, Boolean> equalityValidator) {
+        if (arrays.length == 0) {
+            return Val.ofEmptyArray();
+        }
+
+        var intersection = Val.of(arrays[0].getArrayNode().deepCopy());
+        for (var i = 1; i < arrays.length; i++) {
+            intersection = intersect(intersection, arrays[i], equalityValidator);
+        }
+        return intersection;
+    }
+
+    private static Val intersect(Val array1, Val array2, BiFunction<JsonNode, JsonNode, Boolean> equalityValidator) {
+        var newArray         = Val.JSON.arrayNode();
+        var jsonArray        = array1.getArrayNode();
+        var elementsIterator = jsonArray.elements();
+        while (elementsIterator.hasNext()) {
+            var nextElement = elementsIterator.next();
+            if (contains(nextElement, array2.getArrayNode(), equalityValidator)) {
+                newArray.add(nextElement.deepCopy());
+            }
+        }
+        return Val.of(newArray);
+    }
+
+    private static boolean contains(JsonNode element, ArrayNode array,
+            BiFunction<JsonNode, JsonNode, Boolean> equalityValidator) {
+        var elementsIterator = array.elements();
+        while (elementsIterator.hasNext()) {
+            var nextElement = elementsIterator.next();
+            if (equalityValidator.apply(element, nextElement)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Function(docs = LENGTH_DOC)
     public static Val length(@Array @Text @JsonObject Val parameter) {
