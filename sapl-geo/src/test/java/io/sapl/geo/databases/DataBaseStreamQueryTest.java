85ccdd278b006c660fcc8fef0695025166abd015
AignerManfred <111359589+AignerManfred@users.noreply.github.com>
2024-10-17 11:05:50 +0200

    sapl.geo: databasestreamquerytest: removed reflection tests

--- sapl-geo/src/test/java/io/sapl/geo/databases/DataBaseStreamQueryTest.java
+++ sapl-geo/src/test/java/io/sapl/geo/databases/DataBaseStreamQueryTest.java
@@ -17,17 +17,13 @@
  */
 package io.sapl.geo.databases;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.lang.reflect.InvocationTargetException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.sapl.api.interpreter.PolicyEvaluationException;
 import io.sapl.api.interpreter.Val;
@@ -74,28 +70,6 @@ class DataBaseStreamQueryTest {
 	}
 
 	@Test
-	void getPortTest()
-			throws JsonProcessingException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		var authTemplate = """
-				 {
-				    "user":"test",
-				    "password":"test",
-					"server":"test",
-					"dataBase": "dataBase"
-				 }
-				""";
-		var auth = Val.ofJson(authTemplate).get();
-		var databaseStreamQueryPostGis = new DatabaseStreamQuery(auth, mapper, DataBaseTypes.POSTGIS);
-		var databaseStreamQueryMySql = new DatabaseStreamQuery(auth, mapper, DataBaseTypes.MYSQL);
-		var method = DatabaseStreamQuery.class.getDeclaredMethod("getPort", JsonNode.class);
-		method.setAccessible(true);
-		int portPostGis = (int) method.invoke(databaseStreamQueryPostGis, auth);
-		int portMySql = (int) method.invoke(databaseStreamQueryMySql, auth);
-		assertEquals(5432, portPostGis);
-		assertEquals(3306, portMySql);
-	}
-
-	@Test
 	void getTableErrorTest() throws JsonProcessingException {
 
 		var templateWithoutTable = "{\"geoColumn\":\"test\"}";
@@ -120,96 +94,4 @@ class DataBaseStreamQueryTest {
 		StepVerifier.create(response).expectNext(errorVal).thenCancel().verify();
 	}
 
-	@Test
-	void getColumnsTest()
-			throws JsonProcessingException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		var templateNoArray = "{\"columns\": \"column\"}";
-		var requestNoArray = Val.ofJson(templateNoArray).get();
-		var templateNoColumns = "{}";
-		var requestNoColumns = Val.ofJson(templateNoColumns).get();
-		var auth = Val.ofJson(authenticationTemplate).get();
-		databaseStreamQuery = new DatabaseStreamQuery(auth, mapper, DataBaseTypes.POSTGIS);
-		var method = DatabaseStreamQuery.class.getDeclaredMethod("getColumns", JsonNode.class, ObjectMapper.class);
-		method.setAccessible(true);
-		var resultNoArray = (String[]) method.invoke(databaseStreamQuery, requestNoArray, mapper);
-		var resultNoColumns = (String[]) method.invoke(databaseStreamQuery, requestNoColumns, mapper);
-		assertArrayEquals(new String[] { "column" }, resultNoArray);
-		assertArrayEquals(new String[] {}, resultNoColumns);
-	}
-
-	@Test
-	void getWhereTest()
-			throws JsonProcessingException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		var templateEmpty = "{}";
-		var requestEmpty = Val.ofJson(templateEmpty).get();
-		var template = "{\"where\":\"test\"}";
-		var request = Val.ofJson(template).get();
-		var auth = Val.ofJson(authenticationTemplate).get();
-		databaseStreamQuery = new DatabaseStreamQuery(auth, mapper, DataBaseTypes.POSTGIS);
-		var method = DatabaseStreamQuery.class.getDeclaredMethod("getWhere", JsonNode.class);
-		method.setAccessible(true);
-		var result = (String) method.invoke(databaseStreamQuery, request);
-		var resultEmpty = (String) method.invoke(databaseStreamQuery, requestEmpty);
-		assertEquals("WHERE test", result);
-		assertEquals("", resultEmpty);
-	}
-
-	@Test
-	void getDefaultCrsTest()
-			throws JsonProcessingException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		var templateEmpty = "{}";
-		var requestEmpty = Val.ofJson(templateEmpty).get();
-		var template = "{\"defaultCRS\": 1111}";
-		var request = Val.ofJson(template).get();
-		var auth = Val.ofJson(authenticationTemplate).get();
-		databaseStreamQuery = new DatabaseStreamQuery(auth, mapper, DataBaseTypes.POSTGIS);
-		var method = DatabaseStreamQuery.class.getDeclaredMethod("getDefaultCRS", JsonNode.class);
-		method.setAccessible(true);
-		var result = (int) method.invoke(databaseStreamQuery, request);
-		var resultEmpty = (int) method.invoke(databaseStreamQuery, requestEmpty);
-		assertEquals(1111, result);
-		assertEquals(4326, resultEmpty);
-	}
-
-	@Test
-	void getSingleResultTest()
-			throws JsonProcessingException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		var templateEmpty = "{}";
-		var requestEmpty = Val.ofJson(templateEmpty).get();
-		var templateSingleResult = "{\"singleResult\": true}";
-		var requestSingleResult = Val.ofJson(templateSingleResult).get();
-		var auth = Val.ofJson(authenticationTemplate).get();
-		databaseStreamQuery = new DatabaseStreamQuery(auth, mapper, DataBaseTypes.POSTGIS);
-		var method = DatabaseStreamQuery.class.getDeclaredMethod("getSingleResult", JsonNode.class);
-		method.setAccessible(true);
-		var resultEmpty = (boolean) method.invoke(databaseStreamQuery, requestEmpty);
-		var resultTrue = (boolean) method.invoke(databaseStreamQuery, requestSingleResult);
-		assertEquals(false, resultEmpty);
-		assertEquals(true, resultTrue);
-	}
-
-	@Test
-	void longOrDefaultTest()
-			throws JsonProcessingException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-
-		var auth = Val.ofJson(authenticationTemplate).get();
-		databaseStreamQuery = new DatabaseStreamQuery(auth, mapper, DataBaseTypes.POSTGIS);
-		var templateWithValue = "{\"someField\": 42}";
-		var requestWithValue = Val.ofJson(templateWithValue).get();
-		var templateInvalidValue = "{ \"someField\": \"notANumber\"}";
-		var requestInvalidValue = Val.ofJson(templateInvalidValue).get();
-		var templateWithoutField = "{}";
-		var requestWithoutField = Val.ofJson(templateWithoutField).get();
-		var method = DatabaseStreamQuery.class.getDeclaredMethod("longOrDefault", JsonNode.class, String.class,
-				long.class);
-		method.setAccessible(true);
-		var resultWithValue = (long) method.invoke(databaseStreamQuery, requestWithValue, "someField", 0);
-		var resultWithoutField = (long) method.invoke(databaseStreamQuery, requestWithoutField, "someField", 10);
-		assertEquals(42L, resultWithValue);
-		assertEquals(10L, resultWithoutField);
-		var exception = assertThrows(InvocationTargetException.class,
-				() -> method.invoke(databaseStreamQuery, requestInvalidValue, "someField", 0));
-		var cause = exception.getCause();
-		assertTrue(cause instanceof PolicyEvaluationException);
-	}
 }
