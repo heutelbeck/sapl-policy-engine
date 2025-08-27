fe20cebd76f0e145649f85d6cef2cbe0fee72679
Dominic Heutelbeck <dominic@heutelbeck.com>
2025-08-27 21:04:22 +0200

    use virtual time for wrapper tests

--- sapl-lang/src/test/java/io/sapl/attributes/broker/impl/TimeOutWrapperTests.java
+++ sapl-lang/src/test/java/io/sapl/attributes/broker/impl/TimeOutWrapperTests.java
@@ -42,28 +42,30 @@ class TimeOutWrapperTests {
     void whenFirstElementsAfterTimeOutThenSendTimeOutAndThenTheRestOfTheFlux() {
         var source  = Flux.just(Val.of(1), Val.of(2), Val.of(3)).delayElements(Duration.ofMillis(5L));
         var wrapped = TimeOutWrapper.wrap(source, Duration.ofMillis(1L), TIME_OUT, EMPTY);
-        StepVerifier.create(wrapped).expectNext(TIME_OUT, Val.of(1), Val.of(2), Val.of(3)).verifyComplete();
+        StepVerifier.<Val>withVirtualTime(() -> wrapped).expectNext(TIME_OUT, Val.of(1), Val.of(2), Val.of(3))
+                .verifyComplete();
     }
 
     @Test
     void whenEmptyAfterTimeoutThenSendTimeOutAndEmpty() {
         var source  = Flux.just(Val.of(1)).delayElements(Duration.ofMillis(5L)).filter(v -> false);
         var wrapped = TimeOutWrapper.wrap(source, Duration.ofMillis(1L), TIME_OUT, EMPTY);
-        StepVerifier.create(wrapped).expectNext(TIME_OUT, EMPTY).verifyComplete();
+        StepVerifier.<Val>withVirtualTime(() -> wrapped).expectNext(TIME_OUT, EMPTY).verifyComplete();
     }
 
     @Test
     void whenElementAndClosedThenNoDelayTillWrappedClosed() {
         var source  = Flux.just(Val.of(0));
         var wrapped = TimeOutWrapper.wrap(source, Duration.ofMinutes(1L), TIME_OUT, EMPTY);
-        StepVerifier.create(wrapped).expectNext(Val.of(0)).expectComplete().verify(Duration.ofMillis(10L));
+        StepVerifier.<Val>withVirtualTime(() -> wrapped).expectNext(Val.of(0)).expectComplete()
+                .verify(Duration.ofMillis(10L));
     }
 
     @Test
     void whenErrorThenNoDelayTillWrappedClosed() {
         var source  = Flux.<Val>error(new IllegalStateException("testing"));
         var wrapped = TimeOutWrapper.wrap(source, Duration.ofMinutes(1L), TIME_OUT, EMPTY);
-        StepVerifier.create(wrapped).expectError().verify(Duration.ofMillis(10L));
+        StepVerifier.<Val>withVirtualTime(() -> wrapped).expectError().verify(Duration.ofMillis(10L));
     }
 
 }
