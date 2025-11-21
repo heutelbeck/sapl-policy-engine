fc8f8a5f94aa64eadb8bade589d36fc1a508e58e
Dominic Heutelbeck <dominic@heutelbeck.com>
2025-11-21 22:06:27 +0100

    compiler work

--- sapl-compiler/src/main/java/io/sapl/compiler/CompiledPolicy.java
+++ sapl-compiler/src/main/java/io/sapl/compiler/CompiledPolicy.java
@@ -25,50 +25,7 @@ import io.sapl.api.pdp.Decision;
 import lombok.val;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public record CompiledPolicy(
-        String name,
-        Decision entitlement,
-        CompiledExpression targetExpression,
-        CompiledExpression body,
-        List<CompiledExpression> obligations,
-        List<CompiledExpression> advice,
-        CompiledExpression transformation) {
-
-    public boolean matches(EvaluationContext evaluationContext) {
-        if (BooleanValue.TRUE.equals(targetExpression)) {
-            return true;
-        }
-        if (targetExpression instanceof PureExpression pureTargetExpression) {
-            val targetResult = pureTargetExpression.evaluate(evaluationContext);
-            if (targetResult instanceof BooleanValue booleanValue) {
-                return booleanValue.value();
-            }
-            throw new PolicyEvaluationException(
-                    "Target expression did not return a boolean value, but: %s.".formatted(targetResult));
-        }
-        throw new PolicyEvaluationException(
-                "Target expression not constant or pure expression. Should have been blocked by compilation.");
-    }
-
-    public Flux<AuthorizationDecision> evaluate(EvaluationContext evaluationContext) {
-        val bodyFlux       = ExpressionCompiler.compiledExpressionToFlux(body);
-        val evaluationFlux = bodyFlux.switchMap(bodyResult -> {
-                               if (!(bodyResult instanceof BooleanValue)) {
-                                   return Flux.just(AuthorizationDecision.INDETERMINATE);
-                               }
-                               if (BooleanValue.FALSE.equals(bodyResult)) {
-                                   return Flux.just(AuthorizationDecision.NOT_APPLICABLE);
-                               }
-
-                               // TODO: evaluate the advice, obligations and tranformation via combine latest
-                               // and merge the results into the decisions. If any of them yields an ErrorValue
-                               // return AuthorizationDecision.INDETERMINATE
-                               // create some utility methods for this
-                               return Flux.just(
-                                       new AuthorizationDecision(entitlement, List.of(), List.of(), Value.UNDEFINED));
-                           });
-        return evaluationFlux.contextWrite(ctx -> ctx.put(EvaluationContext.class, evaluationContext));
-    }
-}
+public record CompiledPolicy(String name, PureExpression matchExpression, CompiledExpression decisionExpression) {}
