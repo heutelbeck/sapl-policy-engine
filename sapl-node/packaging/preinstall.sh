5d5c6ed47d1b6739cf61214b0ad35831de95b838
Dominic Heutelbeck <dominic@heutelbeck.com>
2026-03-12 22:59:52 +0100

    fix install scripts

--- sapl-node/packaging/preinstall.sh
+++ sapl-node/packaging/preinstall.sh
@@ -18,5 +18,9 @@
 #
 
 set -e
-chown -R sapl-node:sapl-node /var/lib/sapl-node
-systemctl daemon-reload
+if ! getent group sapl-node >/dev/null 2>&1; then
+    groupadd --system sapl-node
+fi
+if ! getent passwd sapl-node >/dev/null 2>&1; then
+    useradd --system --gid sapl-node --home-dir /var/lib/sapl-node --shell /usr/sbin/nologin --no-create-home sapl-node
+fi
